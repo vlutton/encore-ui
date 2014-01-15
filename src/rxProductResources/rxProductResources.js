464320b4a816f0ec7e15f5a0d2c3f83fab439cfe
Roger Enriquez Jr <roger.enriquez@gmail.com>
2014-01-15 12:12:13 -0600

    fix(rxProductResources): Add correct template URL

--- src/rxProductResources/rxProductResources.js
+++ src/rxProductResources/rxProductResources.js
@@ -2,7 +2,7 @@ angular.module('encore.ui.rxProductResources', [])
     .directive('rxProductResources', function () {
         return {
             restrict: 'E',
-            templateUrl: 'templates/rxProductResources/rxProductResources.html',
+            templateUrl: 'template/rxProductResources/rxProductResources.html',
             scope: {
                 user: '='
             }
