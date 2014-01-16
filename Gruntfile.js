97a8db1c73f709e8dcd0b12258c18cf18a54b134
Roger Enriquez Jr <roger.enriquez@gmail.com>
2014-01-15 19:56:11 -0600

    chore(Grunt): Update bower.json version with task

--- Gruntfile.js
+++ Gruntfile.js
@@ -370,23 +370,32 @@ module.exports = function(grunt) {
   });
 
   function setVersion(type, suffix) {
-    var file = 'package.json';
+    var fileList = ['package.json', 'bower.json'];
     var VERSION_REGEX = /([\'|\"]version[\'|\"][ ]*:[ ]*[\'|\"])([\d|.]*)(-\w+)*([\'|\"])/;
-    var contents = grunt.file.read(file);
-    var version;
-    contents = contents.replace(VERSION_REGEX, function(match, left, center) {
-      version = center;
-      if (type) {
-        version = require('semver').inc(version, type);
-      }
-      //semver.inc strips our suffix if it existed
-      if (suffix) {
-        version += '-' + suffix;
-      }
-      return left + version + '"';
+    var version, contents;
+
+    fileList.forEach(function(file) {
+      contents = grunt.file.read(file);
+
+      contents = contents.replace(VERSION_REGEX, function(match, left, center) {
+        version = center;
+
+        if (type) {
+          version = require('semver').inc(version, type);
+        }
+
+        //semver.inc strips our suffix if it existed
+        if (suffix) {
+          version += '-' + suffix;
+        }
+
+        return left + version + '"';
+      });
+
+      grunt.log.ok('Version set to ' + version.cyan + ' (' + file + ')');
+      grunt.file.write(file, contents);
     });
-    grunt.log.ok('Version set to ' + version.cyan);
-    grunt.file.write(file, contents);
+
     return version;
   }
 
