0a678503e25c17cdd7c3ec2ce45c6781e6c1b2f7
Roger Enriquez Jr <roger.enriquez@gmail.com>
2014-01-15 19:43:11 -0600

    feat(rxNav): Add rxNav and dependencies

---
+++ src/rxNav/rxNav.js
@@ -0,0 +1,51 @@
+angular.module('encore.ui.rxNav', ['encore.ui.rxDropdown'])
+/**
+ *
+ * @ngdoc directive
+ * @name encore.ui.rxNav
+ * @restrict E
+ * @description
+ * TBD
+ */
+.directive('rxNav', function () {
+    return {
+        templateUrl: 'templates/rxNav/rxNav.html',
+        restrict: 'E',
+        controller: function ($scope) {
+            $scope.bookmarks = {
+                linkText: 'Bookmarks',
+                items: [
+                    {
+                        title: 'Bookmarks go here!',
+                        path: '/'
+                    }
+                ]
+            };
+
+            $scope.dashboards = {
+                linkText: 'Dashboards',
+                items: [
+                    {
+                        title: 'Link 1',
+                        path: '/path'
+                    },
+                    {
+                        title: 'Link 2',
+                        path: '/path2'
+                    },
+                    {
+                        title: 'Link 3',
+                        path: '/path3'
+                    }
+                ]
+            };
+
+            $scope.internalTools = {
+                linkText: 'Internal Tools',
+                items: [
+                    { title: 'Ticket Queues', path: '/#/ticketqueues/', className: '' }
+                ]
+            };
+        }
+    };
+});
