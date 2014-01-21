0acfb71e7b402ac7d2bbf49655196299a5e8a971
Kevin Lamping <kevin.lamping@rackspace.com>
2014-01-21 16:52:23 -0600

    chore(grunt): more updates to configure how components are built

--- src/rxForm/example/rx-form.js
+++ src/rxForm/example/rx-form.js
@@ -1,21 +1,19 @@
-angular.module('encore', []);
+angular.module('demo.ui.rxForm', [])
+.controller('rxForm', function ($scope) {
+    $scope.types = [
+        {
+            'value': 'SATA',
+            'label': 'SATA'
+        },
+        {
+            'value': 'SSD',
+            'label': 'SSD'
+        }
+    ];
 
-angular.module('encore')
-    .controller('rxForm', function ($scope) {
-        $scope.types = [
-            {
-                'value': 'SATA',
-                'label': 'SATA'
-            },
-            {
-                'value': 'SSD',
-                'label': 'SSD'
-            }
-        ];
-
-        $scope.volume = {
-            'display_name': 'Volume-1',
-            // select the first type by default
-            'volume_type': $scope.types[0].value
-        };
-    });
+    $scope.volume = {
+        'display_name': 'Volume-1',
+        // select the first type by default
+        'volume_type': $scope.types[0].value
+    };
+});
--- src/rxForm/rx-form.js
+++ src/rxForm/rx-form.js
@@ -1,154 +1,154 @@
-angular.module('encore')
-    /**
-     *
-     * @ngdoc directive
-     * @name encore.directives:rxFormInput
-     * @restrict E
-     * @description
-     * This directive is used to create an &lt;input&gt; tag.
-     * @scope
-     * @param {String} type - Value passed to input's 'type' attribute
-     * @param {String} required - Value passed to input's 'ng-required' attribute
-     * @param {String} fieldId - Used for label and input 'id' attribute
-     * @param {Object} model - Value to bind input to using ng-model
-     * @param {String} minLength - Value passed to input's 'minLength' attribute
-     * @param {String} maxLength - Value passed to input's 'maxLength' attribute
-     * @param {String} max - Value passed to input's 'max' attribute
-     * @param {String} min - Value passed to input's 'min' attribute
-     * @param {String} label - Text to use for <label>
-     * @param {String} suffix - Text to use after <label>
-     * @param {String} description - Text to place after input
-     */
-    .directive('rxFormInput', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/templates/form/rx-form-input.html',
-            scope: {
-                type: '@',
-                required: '@',
-                fieldId: '@',
-                model: '=',
-                minLength: '@',
-                maxLength: '@',
-                max: '@',
-                min: '@',
-                name: '@',
-                value: '@',
-                label: '@',
-                suffix: '@',
-                description: '@'
-            }
-        };
-    })
-    /**
-     *
-     * @ngdoc directive
-     * @name encore.directives:rxFormRadio
-     * @restrict E
-     * @description
-     * This directive is used to create a set of &lt;input type="radio"&gt; tags.
-     * @scope
-     * @param {Array} options - An array of radio inputs to generate. Example format:
-     *     ```
-     *     [{
-     *         label: 'Staging',
-     *         value: 'STAGING'
-     *     }, {
-     *         label: 'ORD (Chicago)',
-     *         value: 'ORD'
-     *     }];
-     *     ```
-     * @param {String} fieldId - Used for label and input 'id' attribute
-     * @param {Object} model - Value to bind input to using ng-model
-     */
-    .directive('rxFormRadio', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/templates/form/rx-form-radio.html',
-            scope: {
-                options: '=',
-                fieldId: '@',
-                model: '='
-            }
-        };
-    })
-    /**
-     *
-     * @ngdoc directive
-     * @name encore.directives:rxFormSelect
-     * @restrict E
-     * @description
-     * This directive is used to create a &lt;select&gt; box w/options.
-     * @scope
-     * @param {Array} options - An array of options to generate. Example format:
-     *     ```
-     *     [{
-     *         label: 'Staging',
-     *         value: 'STAGING'
-     *     }, {
-     *         label: 'ORD (Chicago)',
-     *         value: 'ORD'
-     *     }];
-     *     ```
-     * @param {String} fieldId - Used for label and input 'id' attribute
-     * @param {String} label - Text to use for &lt;label&gt;
-     * @param {Object} model - Value to bind input to using ng-model
-     * @param {String} required - Value passed to select's 'ng-required' attribute
-     */
-    .directive('rxFormSelect', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/templates/form/rx-form-select.html',
-            scope: {
-                options: '=',
-                fieldId: '@',
-                label: '@',
-                model: '=',
-                required: '@'
-            }
-        };
-    })
-    /**
-     *
-     * @ngdoc directive
-     * @name encore.directives:rxFormRadioTable
-     * @restrict E
-     * @description
-     * This directive is used to build a table of radio inputs
-     * @scope
-     * @param {Array} data - Array of objects used to populate table. Should have properties that match columns
-     * key. Example:
-     * [
-     *     {
-     *         'name': 'asv'
-     *     }, {
-     *         'name': 'asdf'
-     *     }, {
-     *         'name': 'av'
-     *     }
-     * ]
-     * @param {array} columns - Array of objects with label/key values. Example:
-     * ```
-     * [{
-     *     'label': 'Name',
-     *     'key': 'name'
-     * }]
-     * @param {String=} selected - Key of item that's selected (and therefore will have input disabled)
-     * @param {String} type - Type of input to be used
-     * @param {Object} model - Value to bind input to using ng-model
-     * @param {String} fieldId - Used for label and input 'id' attribute
-     */
-    .directive('rxFormOptionTable', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/templates/form/rx-form-option-table.html',
-            scope: {
-                data: '=',
-                columns: '=',
-                selected: '@',
-                type: '@',
-                model: '=',
-                fieldId: '@'
-            }
-        };
-    });
+angular.module('encore.ui.rxForm', [])
+/**
+ *
+ * @ngdoc directive
+ * @name encore.directives:rxFormInput
+ * @restrict E
+ * @description
+ * This directive is used to create an &lt;input&gt; tag.
+ * @scope
+ * @param {String} type - Value passed to input's 'type' attribute
+ * @param {String} required - Value passed to input's 'ng-required' attribute
+ * @param {String} fieldId - Used for label and input 'id' attribute
+ * @param {Object} model - Value to bind input to using ng-model
+ * @param {String} minLength - Value passed to input's 'minLength' attribute
+ * @param {String} maxLength - Value passed to input's 'maxLength' attribute
+ * @param {String} max - Value passed to input's 'max' attribute
+ * @param {String} min - Value passed to input's 'min' attribute
+ * @param {String} label - Text to use for <label>
+ * @param {String} suffix - Text to use after <label>
+ * @param {String} description - Text to place after input
+ */
+.directive('rxFormInput', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/templates/form/rx-form-input.html',
+        scope: {
+            type: '@',
+            required: '@',
+            fieldId: '@',
+            model: '=',
+            minLength: '@',
+            maxLength: '@',
+            max: '@',
+            min: '@',
+            name: '@',
+            value: '@',
+            label: '@',
+            suffix: '@',
+            description: '@'
+        }
+    };
+})
+/**
+ *
+ * @ngdoc directive
+ * @name encore.directives:rxFormRadio
+ * @restrict E
+ * @description
+ * This directive is used to create a set of &lt;input type="radio"&gt; tags.
+ * @scope
+ * @param {Array} options - An array of radio inputs to generate. Example format:
+ *     ```
+ *     [{
+ *         label: 'Staging',
+ *         value: 'STAGING'
+ *     }, {
+ *         label: 'ORD (Chicago)',
+ *         value: 'ORD'
+ *     }];
+ *     ```
+ * @param {String} fieldId - Used for label and input 'id' attribute
+ * @param {Object} model - Value to bind input to using ng-model
+ */
+.directive('rxFormRadio', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/templates/form/rx-form-radio.html',
+        scope: {
+            options: '=',
+            fieldId: '@',
+            model: '='
+        }
+    };
+})
+/**
+ *
+ * @ngdoc directive
+ * @name encore.directives:rxFormSelect
+ * @restrict E
+ * @description
+ * This directive is used to create a &lt;select&gt; box w/options.
+ * @scope
+ * @param {Array} options - An array of options to generate. Example format:
+ *     ```
+ *     [{
+ *         label: 'Staging',
+ *         value: 'STAGING'
+ *     }, {
+ *         label: 'ORD (Chicago)',
+ *         value: 'ORD'
+ *     }];
+ *     ```
+ * @param {String} fieldId - Used for label and input 'id' attribute
+ * @param {String} label - Text to use for &lt;label&gt;
+ * @param {Object} model - Value to bind input to using ng-model
+ * @param {String} required - Value passed to select's 'ng-required' attribute
+ */
+.directive('rxFormSelect', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/templates/form/rx-form-select.html',
+        scope: {
+            options: '=',
+            fieldId: '@',
+            label: '@',
+            model: '=',
+            required: '@'
+        }
+    };
+})
+/**
+ *
+ * @ngdoc directive
+ * @name encore.directives:rxFormRadioTable
+ * @restrict E
+ * @description
+ * This directive is used to build a table of radio inputs
+ * @scope
+ * @param {Array} data - Array of objects used to populate table. Should have properties that match columns
+ * key. Example:
+ * [
+ *     {
+ *         'name': 'asv'
+ *     }, {
+ *         'name': 'asdf'
+ *     }, {
+ *         'name': 'av'
+ *     }
+ * ]
+ * @param {array} columns - Array of objects with label/key values. Example:
+ * ```
+ * [{
+ *     'label': 'Name',
+ *     'key': 'name'
+ * }]
+ * @param {String=} selected - Key of item that's selected (and therefore will have input disabled)
+ * @param {String} type - Type of input to be used
+ * @param {Object} model - Value to bind input to using ng-model
+ * @param {String} fieldId - Used for label and input 'id' attribute
+ */
+.directive('rxFormOptionTable', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/templates/form/rx-form-option-table.html',
+        scope: {
+            data: '=',
+            columns: '=',
+            selected: '@',
+            type: '@',
+            model: '=',
+            fieldId: '@'
+        }
+    };
+});
