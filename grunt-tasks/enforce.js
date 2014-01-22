5be4dd8c4053dcb4ccdb2902d638fc7b2c24b2fd
Kevin Lamping <kevin.lamping@rackspace.com>
2014-01-22 14:30:09 -0600

    chore(grunt): removed unneeded file; moved commit-msg.js

--- grunt-tasks/enforce.js
+++ grunt-tasks/enforce.js
@@ -1,8 +1,9 @@
 module.exports = function(grunt) {
     grunt.registerTask('enforce', 'Install commit message enforce script if it doesn\'t exist', function() {
-        if (!grunt.file.exists('.git/hooks/commit-msg')) {
-            grunt.file.copy('misc/validate-commit-msg.js', '.git/hooks/commit-msg');
-            require('fs').chmodSync('.git/hooks/commit-msg', '0755');
-        }
+        // commented out since not overwriting means updates will never be recieved
+        //if (!grunt.file.exists('.git/hooks/commit-msg')) {
+        grunt.file.copy('./grunt-tasks/misc/validate-commit-msg.js', '.git/hooks/commit-msg');
+        require('fs').chmodSync('.git/hooks/commit-msg', '0755');
+        //}
     });
 };
