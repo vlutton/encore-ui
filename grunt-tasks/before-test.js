07390db9c56dc454685294948bc027f386fef822
Kevin Lamping <kevin.lamping@rackspace.com>
2014-01-28 13:14:29 -0600

    chore(travis): fix travis ci build script; added license

--- grunt-tasks/before-test.js
+++ grunt-tasks/before-test.js
@@ -1,3 +1,3 @@
 module.exports = function(grunt) {
-    grunt.registerTask('before-test', ['clean:coverage', 'enforce', 'jshint']);
+    grunt.registerTask('before-test', ['clean:coverage', 'jshint']);
 };
--- grunt-tasks/options/shell.js
+++ grunt-tasks/options/shell.js
@@ -1,3 +1,4 @@
+/*jshint node:true*/
 module.exports = {
     //We use %version% and evluate it at run-time, because <%= pkg.version %>
     //is only evaluated once
@@ -21,10 +22,15 @@ module.exports = {
         ].join(' && ')
     },
     hooks: {
-        command: [
-            'rm -f .git/hooks/jshint .git/hooks/validate-commit-msg',
-            'cp grunt-tasks/git-hooks/* .git/hooks/',
-            'chmod u+x .git/hooks/jshint .git/hooks/validate-commit-msg'
-        ].join(' && ')
+        command: function () {
+            // don't run the command if we're in Travis CI
+            if (!process.env.TRAVIS) {
+                return [
+                    'rm -f .git/hooks/jshint .git/hooks/validate-commit-msg',
+                    'cp grunt-tasks/git-hooks/* .git/hooks/',
+                    'chmod u+x .git/hooks/jshint .git/hooks/validate-commit-msg'
+                ].join(' && ');
+            }
+        }
     }
 };
