1237332dc630c095cf1185ee46a4f52864af9caa
Kevin Lamping <kevin.lamping@rackspace.com>
2014-02-04 17:50:18 -0600

    chore(travis): added new grunt-bump; added deploy info to travis

--- grunt-tasks/options/shell.js
+++ grunt-tasks/options/shell.js
@@ -1,26 +1,5 @@
 /*jshint node:true*/
 module.exports = {
-    //We use %version% and evluate it at run-time, because <%= pkg.version %>
-    //is only evaluated once
-    'release-prepare': {
-        command: [
-            'grunt before-test after-test',
-            'grunt version', //remove "-SNAPSHOT"
-            'grunt changelog'
-        ].join(' && ')
-    },
-    'release-complete': {
-        command: [
-            'git commit CHANGELOG.md bower.json package.json -m "chore(release): v%version%"',
-            'git tag %version%'
-        ].join(' && ')
-    },
-    'release-start': {
-        command: [
-            'grunt version:minor:"SNAPSHOT"',
-            'git commit bower.json package.json -m "chore(release): Starting v%version%"'
-        ].join(' && ')
-    },
     hooks: {
         command: function () {
             // don't run the command if we're in Travis CI
---
+++ grunt-tasks/shipit.js
@@ -0,0 +1,26 @@
+/*jshint node:true */
+module.exports = function (grunt) {
+    grunt.registerTask('shipit', 'Ships code to prod', function (versionType) {
+        var validTypes = ['major', 'minor', 'patch'];
+
+        if (validTypes.indexOf(versionType) > -1) {
+            // increment the version
+            grunt.task.run('bump-only:' + versionType);
+
+            // create changelog
+            grunt.task.run('changelog');
+
+            // commit version increment
+            grunt.task.run('bump-commit');
+
+            // build the code
+            grunt.task.run('before-test');
+            grunt.task.run('after-test');
+
+            // push files to prod
+            grunt.task.run('cloudfiles:production');
+        } else {
+            grunt.fatal('Must pass in version type major/minor/patch. E.g. `grunt shipit:patch`');
+        }
+    });
+};
