9d5fa808062a51a96d2a0e408fce4b0ab420554e
Droogans <droogans@gmail.com>
2014-02-12 16:53:40 -0600

    feat(grunt): Sample random shipit squirrels.

--- grunt-tasks/squirrel.js
+++ grunt-tasks/squirrel.js
@@ -1,20 +1,32 @@
 /*jshint node:true */
+var _ = require('lodash');
+
 module.exports = function (grunt) {
     grunt.registerTask('squirrel', 'Logs a shipit squirrel', function () {
-        var squirrel =
-            '         _-------_        _______________\n' +
-            '       .\'  ..::. `\\      /               \\\n' +
-            '      /  .::\' `\'` /     | Ship it! v<%= pkg.version %> |\n' +
-            '     / .::\' .--.=;     / ________________/\n' +
-            '     | ::\' /  C ..\\   / /\n' +
-            '     | :: |   \\  _.) <_/\n' +
-            '      \\ \':|   /  \\\n' +
-            '       \'-, \\./ \\)\\)\n' +
-            '          `-|   );/\n' +
-            '             \'--\'-\'\'';
+        var squirrels = [
+            ['         _-------_        _______________',
+             '       .\'  ..::. `\\      /               \\',
+             '      /  .::\' `\'` /     | Ship it! v<%= pkg.version %> |',
+             '     / .::\' .--.=;     / ________________/',
+             '     | ::\' /  C ..\\   / /',
+             '     | :: |   \\  _.) <_/',
+             '      \\ \':|   /  \\',
+             '       \'-, \\./ \\)\\)',
+             '          `-|   );/',
+             '             \'--\'-\'\''],
+            ['         _-------_        _______________',
+             '       .\'  ..::. `\\      /               \\',
+             '      /  .::\' `\'` /     | Ship it! v<%= pkg.version %> |',
+             '     / .::\' .--.=;     / ________________/',
+             '     | ::\' /  C ..\\   / /',
+             '     | :: |   \\  _.) <_/',
+             '      \\ \':|   /  \\',
+             '       \'-, \\./ \\)\\)',
+             '          `-|   );/',
+             '             \'--\'-\'\'']
+        ];
 
-
-        squirrel = grunt.template.process(squirrel);
+        var squirrel = grunt.template.process(_.sample(squirrels).join('\n'));
         console.log(squirrel);
     });
 };
