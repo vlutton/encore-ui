b639081e7acbf209b244c9b0c9d6ede75b1a5b65
Kevin Lamping <kevin.lamping@rackspace.com>
2014-02-13 14:49:05 -0600

    feat(css): now minifying css

--- grunt-tasks/after-test.js
+++ grunt-tasks/after-test.js
@@ -1,3 +1,4 @@
 module.exports = function (grunt) {
-    grunt.registerTask('after-test', ['html2js', 'build', 'copy', 'rxPageObjects', 'clean:templates', 'complexity']);
+    grunt.registerTask('after-test', ['html2js', 'build', 'copy', 'rxPageObjects', 'cssmin', 'clean:templates',
+        'complexity']);
 };
