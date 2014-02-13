53bee52cccba2999e027dd799e72e2e36195e8b4
Kevin Lamping <kevin.lamping@rackspace.com>
2014-02-13 16:54:16 -0600

    test(js): cannot run minified tests before files are minified

--- grunt-tasks/test.js
+++ grunt-tasks/test.js
@@ -2,7 +2,5 @@
 module.exports = function (grunt) {
     grunt.registerTask('test', 'Run tests on karma server', function () {
         grunt.task.run('karma:single');
-        // make sure to run tests against minified files as well
-        grunt.task.run('karma:minified');
     });
 };
