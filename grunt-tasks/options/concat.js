cf000ab35d51f808f26f436fc6b437d36d8e3111
Kevin Lamping <kevin.lamping@rackspace.com>
2014-02-13 10:35:33 -0600

    chore(midway): fix output of page objects; ignore generated files

--- grunt-tasks/options/concat.js
+++ grunt-tasks/options/concat.js
@@ -1,3 +1,5 @@
+var pageObjectHeader = '/*jshint node:true*/\nvar Page = require(\'astrolabe\').Page;\n';
+
 module.exports = {
     dist: {
         options: {
@@ -14,6 +16,13 @@ module.exports = {
         dest: '<%= config.dist %>/<%= config.fileName %>-tpls.js'
     },
     rxPageObjects: {
+        options: {
+            // Replace all Astrolabe requires with a single one up top
+            banner: pageObjectHeader,
+            process: function (src) {
+                return src.replace(pageObjectHeader, '');
+            }
+        },
         src: ['src/*/*.page.js'],
         dest: 'index.js'
     }
