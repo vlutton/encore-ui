db156c4e6c3e9d4823b11c07687dd9a7ed1aa651
Kevin Lamping <kevin.lamping@rackspace.com>
2014-02-14 15:08:59 -0600

    feat(rxSortableColumn): new component; added "visually hidden" class for accessibility

---
+++ src/rxSortableColumn/docs/rxSortableColumn.js
@@ -0,0 +1,30 @@
+/*jshint unused:false*/
+
+// This file is used to help build the 'demo' documentation page and should be updated with example code
+function rxSortableColumnCtrl ($scope, PageTracking, rxSortUtil) {
+    $scope.sort = rxSortUtil.getDefault();
+    $scope.pager = PageTracking.createInstance();
+
+    $scope.sortCol = function (predicate) {
+        return rxSortUtil.sortCol($scope, predicate);
+    };
+
+    $scope.talentPool = [
+        {
+            name: 'Andrew Yurisich',
+            jobTitle: 'Mailroom Associate IV'
+        },
+        {
+            name: 'Patrick Deuley',
+            jobTitle: 'Design Chaplain'
+        },
+        {
+            name: 'Hussam Dawood',
+            jobTitle: 'Evangelist of Roger Enriquez'
+        },
+        {
+            name: 'Kerry Bowley',
+            jobTitle: 'Dev Mom'
+        },
+    ];
+}
---
+++ src/rxSortableColumn/rxSortableColumn.spec.js
@@ -0,0 +1,70 @@
+/* jshint node: true */
+
+describe('rxSortableColumn', function () {
+    var scope, compile, rootScope, el;
+    var validTemplate = '<rx-sortable-column reverse="true">Yo!</rx-sortable-column>';
+
+    beforeEach(function () {
+        // load module
+        module('encore.ui.rxSortableColumn');
+
+        // load templates
+        module('templates/rxSortableColumn.html');
+
+        // Inject in angular constructs
+        inject(function ($location, $rootScope, $compile) {
+            rootScope = $rootScope;
+            scope = $rootScope.$new();
+            compile = $compile;
+        });
+
+        el = helpers.createDirective(validTemplate, compile, scope);
+    });
+
+    afterEach(function () {
+        el = null;
+    });
+
+    it('should render template correctly', function () {
+        expect(el).not.be.empty;
+        expect(el.find('button')).not.be.empty;
+        expect(el.find('button').text()).is.equal('Yo!');
+        expect(el.find('i')).not.be.empty;
+        expect(el.find('i').hasClass('asc')).to.be.true;
+    });
+});
+
+describe('rxSortUtil', function () {
+    var util;
+
+    beforeEach(function () {
+        module('encore.ui.rxSortableColumn');
+
+        inject(function (rxSortUtil) {
+            util = rxSortUtil;
+        });
+    });
+
+    it('SortUtil: getDefault will return default settings when no parameters are passed', function () {
+        expect(util.getDefault()).to.deep.eq({ predicate: 'name', reverse: undefined });
+    });
+
+    it('SortUtils: getDfault will set parameters if specified', function () {
+        expect(util.getDefault('datacenter', true)).to.deep.eq({ predicate: 'datacenter', reverse: true });
+    });
+
+    it('SortUtil: sortCol will sort reverse the sort if it is the same predicate', function () {
+        var scope = { sort: { predicate: 'name', reverse: false }, pager: { pageNumber: 1 }};
+
+        util.sortCol(scope, 'name');
+        expect(scope.sort.reverse).to.be.true;
+        expect(scope.pager.pageNumber).to.eq(0);
+    });
+
+    it('SortUtil: sortCol will set reverse to false if it is different predicate', function () {
+        var scope = { sort: { predicate: 'name', reverse: true }, pager: { pageNumber: 1 }};
+
+        util.sortCol(scope, 'datacenter');
+        expect(scope.sort.reverse).to.be.false;
+    });
+});
