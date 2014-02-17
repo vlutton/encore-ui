b448374a9e2e7211dd2f88824bb82a29f92eca5c
Droogans <droogans@gmail.com>
2014-02-17 16:34:44 -0600

    test(midway): More sortable column UI tests.

--- src/rxSortableColumn/docs/rxSortableColumn.midway.js
+++ src/rxSortableColumn/docs/rxSortableColumn.midway.js
@@ -1,52 +1,51 @@
 /*jshint undef:false*/
 
 var demoPage = require('../../../utils/demo.page.js');
-var rxSortableColumnPage = require('../rxSortableColumn.page.js').rxSortableColumn;
+var rxSortableColumn = require('../rxSortableColumn.page.js').rxSortableColumn;
 var expect = require('chai').use(require('chai-as-promised')).expect;
-var Page = require('astrolabe').Page;
-
-var examplePage = Page.create({
-    resourceTable: {
-        get: function () {
-            var repeater = 'resource in talentPool | orderBy:sort.predicate:sort.reverse';
-            return this.findElements(this.by.repeater(repeater));
-        }
-    },
-});
 
 // Add midway tests to run
 describe('rxSortableColumn', function () {
-    var ptor = rxSortableColumnPage.driver;
+    var ptor = protractor.getInstance();
+    var nameColumn = null;
+    var roleColumn = null;
 
     before(function () {
         demoPage.go();
+        var nameSelector = ptor.findElement(protractor.By.css('rx-sortable-column[sort-property="name"]'));
+        var roleSelector = ptor.findElement(protractor.By.css('rx-sortable-column[sort-property="jobTitle"]'));
+        nameColumn = rxSortableColumn.initialize(nameSelector);
+        roleColumn = rxSortableColumn.initialize(roleSelector);
     });
 
-    it('should show element', function () {
-        expect(rxSortableColumnPage.rootElement.isDisplayed()).to.eventually.eq.true;
+    it('should display some sortable columns', function () {
+        expect(nameColumn.rootElement.isDisplayed()).to.eventually.eq.true;
+        expect(roleColumn.rootElement.isDisplayed()).to.eventually.eq.true;
     });
 
-    it('should sort descending by default', function () {
-        expect(rxSortableColumnPage.sortIcon.getAttribute('class')).to.eventually.contain('desc');
-        examplePage.resourceTable.then(function (rows) {
-            // expect first resource to be Andrew
-            expect(rows[0].getText()).to.eventually.contain('Yurisich');
-
-            // expect last resource to be Patrick
-            expect(rows[rows.length - 1].getText()).to.eventually.contain('Deuley');
-        });
+    it('should have a descending sort shown by default for the name column', function () {
+        expect(nameColumn.getCurrentSortDirection()).to.eventually.eq(0);
     });
 
-    it('should sort ascending after toggle', function () {
-        // toggle sort
-        rxSortableColumnPage.sortAction.click();
-
-        expect(rxSortableColumnPage.sortIcon.getAttribute('class')).to.eventually.contain('asc');
-
-        // expect first resource to be Patrick
-        examplePage.resourceTable.then(function (rows) {
-            expect(rows[0].getText()).to.eventually.contain('Deuley');
-            expect(rows[rows.length - 1].getText()).to.eventually.contain('Yurisich');
-        });
+    it('should have no sort shown by default for the job title column', function () {
+        expect(roleColumn.getCurrentSortDirection()).to.eventually.eq(-1);
     });
+
+    it('should support sorting columns ascending', function () {
+        var isAscending = true;
+        nameColumn.sort(isAscending);
+        expect(nameColumn.getCurrentSortDirection()).to.eventually.eq(1);
+    });
+
+    it('should support sorting columns descending', function () {
+        var isAscending = false;
+        nameColumn.sort(isAscending);
+        expect(nameColumn.getCurrentSortDirection()).to.eventually.eq(0);
+    });
+
+    it('should remove all other sorts when sorting an unsorted column', function () {
+        roleColumn.sort();
+        expect(nameColumn.getCurrentSortDirection()).to.eventually.eq(-1);
+    });
+
 });
--- src/rxSortableColumn/rxSortableColumn.page.js
+++ src/rxSortableColumn/rxSortableColumn.page.js
@@ -1,21 +1,69 @@
 /*jshint node:true*/
 var Page = require('astrolabe').Page;
 
-exports.rxSortableColumn = Page.create({
-    // Elements
-    rootElement: {
+var page = {
+
+    btnSort: {
         get: function () {
-            return this.findElement(this.by.css('.rx-sortable-column'));
+            return this.rootElement.findElement(this.by.css('.sort-action'));
         }
     },
-    sortAction: {
+
+    imgSortIcon: {
         get: function () {
-            return this.findElement(this.by.css('.rx-sortable-column .sort-action'));
+            return this.rootElement.findElement(this.by.css('.sort-icon'));
         }
     },
-    sortIcon: {
-        get: function () {
-            return this.findElement(this.by.css('.rx-sortable-column .sort-icon'));
+
+    sort: {
+        // Will default to an ascending sort if no parameters are passed in.
+        value: function (isAscending) {
+            if (isAscending === undefined) {
+                isAscending = true;
+            }
+
+            var page = this;
+            return this.getCurrentSortDirection().then(function (sortDirection) {
+                /*jshint eqeqeq: false*/
+                // Coercing -1 to Boolean results in -1 === true. We don't want that.
+                // It's easier to leave as is since -1 != true and -1 != false.
+                // Meaning we'll always sort the list at least once if it's currently unsorted.
+                if (sortDirection != isAscending) {
+                    page.btnSort.click();
+                    page.sort(isAscending);
+                }
+            });
+        }
+    },
+
+    getCurrentSortDirection: {
+        // Ascending sort:  (1)  means the arrow is pointed down. [0-9, a-z]
+        // Descending sort: (0)  means the arrow is pointed up.   [z-a, 9-0]
+        // Not sorted:     (-1)  means there is no arrow for this column.
+        value: function () {
+            var page = this;
+            return this.imgSortIcon.getAttribute('style').then(function (style) {
+                if (style.indexOf('hidden') > -1) {
+                    // Sort arrow hidden; not sorted.
+                    return -1;
+                } else {
+                    return page.imgSortIcon.getAttribute('class').then(function (className) {
+                        return className.indexOf('asc') > -1 ? 1 : 0;
+                    });
+                }
+            });
         }
     }
-});
+
+};
+
+exports.rxSortableColumn = {
+
+    initialize: function (rxSortableColumnElement) {
+        page.rootElement = {
+            get: function () { return rxSortableColumnElement; }
+        };
+        return Page.create(page);
+    }
+
+};
