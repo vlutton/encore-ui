80ee5da9926ac54611f24fca5f2f953e0ff519cc
Kevin Lamping <kevin.lamping@rackspace.com>
2014-02-20 12:55:43 -0600

    docs(rxNotify): adding docs for rxNotify

--- src/rxNotify/rxNotify.js
+++ src/rxNotify/rxNotify.js
@@ -1,5 +1,11 @@
 /*jshint undef:false*/
 angular.module('encore.ui.rxNotify', [])
+/*
+ * Displays all messages in a stack
+ * @ngdoc directive
+ * @example <rx-notifications stack="myStack"></rx-notifications>
+ * @scope
+ */
 .directive('rxNotifications', function (rxNotify) {
     return {
         scope: {
@@ -9,6 +15,10 @@ angular.module('encore.ui.rxNotify', [])
         replace: true,
         templateUrl: 'templates/rxNotifications.html',
         controller: function ($scope) {
+            /*
+             * Calls rxNotify service to remove a message from a stack
+             * @param {object} message The message object to remove.
+             */
             $scope.dismiss = function (message) {
                 rxNotify.dismiss(message);
             };
@@ -16,6 +26,7 @@ angular.module('encore.ui.rxNotify', [])
         link: function ($scope) {
             var stack = $scope.stack || 'page';
 
+            // watch the stack for updates
             $scope.$watch(function () {
                 return rxNotify.stacks[stack];
             }, function (data) {
@@ -39,10 +50,15 @@ angular.module('encore.ui.rxNotify', [])
         timeout: -1,
         dismissable: true,
         show: 'immediate',
-        dismiss: 'routeChange',
+        dismiss: 'next',
         stack: 'page'
     };
 
+    /*
+     * Adds a message to a stack
+     * @private
+     * @param {object} message The message object to add.
+     */
     var addToStack = function (message) {
         // if timeout is set, we should remove message after time expires
         if (message.timeout > -1) {
@@ -52,6 +68,11 @@ angular.module('encore.ui.rxNotify', [])
         stacks[message.stack].push(message);
     };
 
+    /*
+     * Sets a timeout to wait a specific time then dismiss message
+     * @private
+     * @param {object} message The message object to remove.
+     */
     var dismissAfterTimeout = function (message) {
         // convert seconds to milliseconds
         var timeoutMs = message.timeout * 1000;
@@ -61,9 +82,25 @@ angular.module('encore.ui.rxNotify', [])
         }, timeoutMs);
     };
 
-    var changeOnWatch = function (message, type, cb) {
-        var scope = message[type][0];
-        var prop = message[type][1];
+    /*
+     * Shows/dismisses message after scope.prop change to true
+     * @private
+     * @param {object} message The message object to show/dismiss
+     * @param {string} changeType Whether to 'show' or 'dismiss' the message
+     */
+    var changeOnWatch = function (message, changeType) {
+        var scope = message[changeType][0];
+        var prop = message[changeType][1];
+
+        // function to run to change message visibility
+        var cb;
+
+        // switch which function to call based on type
+        if (changeType == 'show') {
+            cb = addToStack;
+        } else if (changeType == 'dismiss') {
+            cb = dismiss;
+        }
 
         scope.$watch(prop, function (newVal) {
             if (newVal === true) {
@@ -72,13 +109,49 @@ angular.module('encore.ui.rxNotify', [])
         });
     };
 
-    // deletes all messages in a stack
+    /*
+     * removes all messages that are shown and dismissable
+     * @private
+     */
+    var clearAllDismissable = function () {
+        _.forOwn(stacks, function (index, key) {
+            stacks[key] = _.reject(stacks[key], {
+                'dismiss': messageDefaults.dismiss
+            });
+        });
+    };
+
+    /*
+     * adds messages marked as 'next' to relevant queues
+     * @private
+     */
+    var addAllNext = function () {
+        _.each(nextQueue, function (message) {
+            // add to appropriate stack
+            addToStack(message);
+        });
+
+        // empty nextQueue of messages
+        nextQueue.length = 0;
+    };
+
+    /*
+     * deletes all messages in a stack
+     * @public
+     * @param {string} stack The name of the stack to clear
+     */
     var clear = function (stack) {
         // @see http://davidwalsh.name/empty-array
         stacks[stack].length = 0;
     };
 
-    // removes a specific message from a stack
+    /*
+     * removes a specific message from a stack
+     * @public
+     * @param {string} messageId The ID of the message to remove.
+     * @param {string} stack The name of the stack the message is it
+     * @note Alternatively, you can pass in a message object and the ID/Stack will be determined from the object
+     */
     var dismiss = function (messageId, stack) {
         // handle if message passed in (versus id)
         if (_.isObject(messageId)) {
@@ -91,7 +164,12 @@ angular.module('encore.ui.rxNotify', [])
         stacks[stack] = _.reject(stacks[stack], { 'id': messageId });
     };
 
-    // adds a message to a stack
+    /*
+     * adds a message to a stack
+     * @public
+     * @param {string} text Message text
+     * @param {object} options Message options. See README.md for examples
+     */
     var add = function (text, options) {
         var message = {
             text: text
@@ -118,7 +196,7 @@ angular.module('encore.ui.rxNotify', [])
 
         // if dismiss is set to array, watch variable
         if (_.isArray(message.dismiss)) {
-            changeOnWatch(message, 'dismiss', dismiss);
+            changeOnWatch(message, 'dismiss');
         }
 
         // add message to stack immediately if has default show value
@@ -127,33 +205,14 @@ angular.module('encore.ui.rxNotify', [])
         } else if (message.show == 'next') {
             nextQueue.push(message);
         } else if (_.isArray(message.show)) {
-            changeOnWatch(message, 'show', addToStack);
+            changeOnWatch(message, 'show');
         }
 
         // return id for tracking
         return message.id;
     };
 
-    // removes all messages that are shown and dismissable
-    var clearAllDismissable = function () {
-        _.forOwn(stacks, function (index, key) {
-            stacks[key] = _.reject(stacks[key], {
-                'dismiss': messageDefaults.dismiss
-            });
-        });
-    };
-
-    // adds messages marked as 'next' to relevant queues
-    var addAllNext = function () {
-        _.each(nextQueue, function (message) {
-            // add to appropriate stack
-            addToStack(message);
-        });
-
-        // empty nextQueue of messages
-        nextQueue.length = 0;
-    };
-
+    // add a listener to root scope which listens for the event that gets fired when the route successfully changes
     $rootScope.$on('$routeChangeSuccess', function processRouteChange () {
         clearAllDismissable();
         addAllNext();
