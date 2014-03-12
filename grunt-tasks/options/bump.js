c0cda8600441ee8f144704a523a52f7d61e82615
Jay Parlar <parlar@gmail.com>
2014-03-12 16:37:19 -0500

    fix(grunt): Change docs to guides in bump.js

--- grunt-tasks/options/bump.js
+++ grunt-tasks/options/bump.js
@@ -4,7 +4,7 @@ module.exports = {
         updateConfigs: ['pkg'],
         commit: true,
         commitMessage: 'chore(version): v%VERSION% [skip ci]',
-        commitFiles: ['package.json', 'bower.json', 'CHANGELOG.md', 'README.md', 'docs/testing.md',
+        commitFiles: ['package.json', 'bower.json', 'CHANGELOG.md', 'README.md', 'guides/testing.md',
             'utils/rx-page-objects/package.json'],
         createTag: true,
         tagName: 'v%VERSION%',
