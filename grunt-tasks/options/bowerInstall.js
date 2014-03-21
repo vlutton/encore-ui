a9c5471b4af9f28977384c136242c57f625d3e99
Kevin Lamping <kevin.lamping@rackspace.com>
2014-03-21 12:01:09 -0500

    chore(grunt): add dep injection into karma.js

--- grunt-tasks/options/bowerInstall.js
+++ grunt-tasks/options/bowerInstall.js
@@ -1,7 +1,21 @@
 module.exports = {
-    target: {
-        src: [
-            'demo/index.html'
-        ]
+    demoHtml: {
+        src: 'demo/index.html'
+    },
+    karmaConfig: {
+        src: ['grunt-tasks/options/karma.js'],
+        devDependencies: true,
+        ignorePath: '../../',
+        fileTypes: {
+            js: {
+                block: /(([ \t]*)\/\/\s*bower:*(\S*))(\n|\r|.)*?(\/\/\s*endbower)/gi,
+                detect: {
+                    js: /['"](.+)['"],/gi
+                },
+                replace: {
+                    js: '\'{{filePath}}\','
+                }
+            }
+        }
     }
 };
