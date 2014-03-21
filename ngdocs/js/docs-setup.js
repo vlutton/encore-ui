b8a33e62bdb767bcd985e77f42ccd58e5c6e0125
Kevin Lamping <kevin.lamping@rackspace.com>
2014-03-21 17:03:27 -0500

    docs(ghpages): release v0.3.4

---
+++ ngdocs/js/docs-setup.js
@@ -0,0 +1,177 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "encore.components.paginate:Page",
+      "shortName": "Page",
+      "type": "filter",
+      "moduleName": "encore.components",
+      "shortDescription": "This is the pagination filter that is used to limit the number of pages",
+      "keywords": "api components created displayed encore filter instance limit list number numbers pager pagetracking paginate pagination service"
+    },
+    {
+      "section": "api",
+      "id": "encore.components.paginate:PageTracking",
+      "shortName": "PageTracking",
+      "type": "service",
+      "moduleName": "encore.components",
+      "shortDescription": "This is the data service that can be used in conjunction with the pagination",
+      "keywords": "ability api class components conjunction controls createinstance current data default determine display enables encore encore-components-paginate-page encore-components-paginate-pagetracking-page generate initialzed instance items itemsperpage number object objects override pageinit pagenumber pagestoshow pagetracking paginate pagination service set setting settings showall store stored tables total true"
+    },
+    {
+      "section": "api",
+      "id": "encore.components.paginate:Paginate",
+      "shortName": "Paginate",
+      "type": "filter",
+      "moduleName": "encore.components",
+      "shortDescription": "This is the pagination filter that is used to calculate the division in the",
+      "keywords": "api calculate components created current division encore filter instance items list object pager pagetracking paginate pagination paging service sliced"
+    },
+    {
+      "section": "api",
+      "id": "encore.components.paginate:rxPaginate",
+      "shortName": "rxPaginate",
+      "type": "directive",
+      "moduleName": "encore.components",
+      "shortDescription": "Directive that takes in the page tracking object and outputs a page",
+      "keywords": "api components controller directive display encore instance maximum number numberofpages object outputs pagetracking paginate service switching takes time tracking"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxActiveUrl",
+      "shortName": "encore.ui.rxActiveUrl",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "This is used as an attribute to directive , in the",
+      "keywords": "api attribute checks class current directive encore form returns rx-active-url rxactiveurl selected subset ui url"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxButton:rxButton",
+      "shortName": "rxButton",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "Renders a button which will disable when clicked and show a loading message",
+      "keywords": "api button clicked complete default defaultmsg directive disable display displayed encore loading loadingmsg message operation progress renable renders rxbutton text toggle true ui"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxForm:rxFormInput",
+      "shortName": "rxFormInput",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "This directive is used to create an &lt;input&gt; tag.",
+      "keywords": "api attribute bind create description directive encore fieldid input label max maxlength min minlength model ng-model ng-required passed place required rxform suffix tag text type ui"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxForm:rxFormOptionTable",
+      "shortName": "rxFormOptionTable",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "This directive is used to build a table of radio/checkbox inputs",
+      "keywords": "api array attribute bind build columns data directive disabled encore example fieldid input inputs item key label match model ng-model objects option populate properties radio rxform selected table type ui values"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxForm:rxFormRadio",
+      "shortName": "rxFormRadio",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "This directive is used to create a set of &lt;input type=&quot;radio&quot;&gt; tags.",
+      "keywords": "api array attribute bind create directive encore example fieldid format generate input inputs label model ng-model options radio rxform set staging tags type ui"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxForm:rxFormSelect",
+      "shortName": "rxFormSelect",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "This directive is used to create a &lt;select&gt; box w/options.",
+      "keywords": "api array attribute bind box create directive encore example fieldid format generate input label model ng-model ng-required options passed required rxform select staging text ui"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxLocalStorage:LocalStorage",
+      "shortName": "LocalStorage",
+      "type": "service",
+      "moduleName": "encore.ui",
+      "shortDescription": "A simple wrapper for injecting the global variable localStorage",
+      "keywords": "$document $window angular api batman clear encore functions getitem getobject global helper hero http includes injecting key local localstorage objects org provided removeitem return returns robin rxlocalstorage service services setitem setobject setting simple specification storage storage-0 storing ui values variable w3 w3c works wrapper"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxNav",
+      "shortName": "encore.ui.rxNav",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "TBD",
+      "keywords": "api directive encore rxnav tbd ui"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxProductResources",
+      "shortName": "encore.ui.rxProductResources",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "TBD",
+      "keywords": "api directive encore rxproductresources tbd ui"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxRelatedMenu",
+      "shortName": "encore.ui.rxRelatedMenu",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "This directive is used to create a menu for a page. By default it shows",
+      "keywords": "api click closed collapsable configured create default default-state direction directive encore going hover left live menu open opened option position potential rxrelatedmenu side sidebar trigger true ui values"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxSessionStorage:SessionStorage",
+      "shortName": "SessionStorage",
+      "type": "service",
+      "moduleName": "encore.ui",
+      "shortDescription": "A simple wrapper for injecting the global variable sessionStorage",
+      "keywords": "$document $window angular api batman clear encore functions getitem getobject global helper hero http includes injecting key objects org provided removeitem return returns robin rxsessionstorage service services session sessionstorage setitem setobject setting simple specification storage storage-0 storing ui values variable w3 w3c works wrapper"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxSortableColumn",
+      "shortName": "encore.ui.rxSortableColumn",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "Renders a clickable link in a table heading which will sort the table by",
+      "keywords": "api array ascending called clickable clicked collection current descending directive displayed displaytext encore function heading indicates link order predicate property referenced renders reverse rxsortablecolumn sort sorted sortmethod sortproperty table text ui"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxSortUtil",
+      "shortName": "encore.ui.rxSortUtil",
+      "type": "service",
+      "moduleName": "encore.ui",
+      "shortDescription": "Service which provided utility methods for sorting collections.",
+      "keywords": "api based collection collections default encore getdefault methods object predicate provided returns rxsortutil service sort sortcol sorting sorts ui utility"
+    },
+    {
+      "section": "api",
+      "id": "encore.ui.rxSpinner:rxSpinner",
+      "shortName": "rxSpinner",
+      "type": "directive",
+      "moduleName": "encore.ui",
+      "shortDescription": "Renders a spinner animation on the provided element given the &#39;toggle&#39; attribute is truthy",
+      "keywords": "animation api attribute controls default directive display element encore extra-large large mini options provided renders rxspinner size small spinner toggle true truthy ui"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
