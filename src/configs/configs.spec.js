e26be311b7546bb34b4e95f606bba524c320cf74
Kevin Lamping <kevin.lamping@rackspace.com>
2014-03-26 18:54:22 -0500

    chore(rxApp): fix failing config test

--- src/configs/configs.spec.js
+++ src/configs/configs.spec.js
@@ -1,21 +1,19 @@
 /* jshint node: true */
 
 describe('encore.ui.configs', function () {
-    var routes, dp;
+    var dp;
 
     beforeEach(function () {
         // Load the directive's module
         module('encore.ui.configs');
 
         // Inject in angular constructs
-        inject(function (ROUTE_PATHS, devicePaths) {
-            routes = ROUTE_PATHS;
+        inject(function (devicePaths) {
             dp = devicePaths;
         });
     });
 
     it('should exist', function () {
-        expect(routes).to.exist;
         expect(dp).to.exist;
     });
 });
