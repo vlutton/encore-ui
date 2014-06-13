6eaf86be2dd2318da06e3ae968363ab650b5b461
Ernie Hsiung <ernie.hsiung@rackspace.com>
2014-06-13 13:41:20 -0400

    docs(css): Replace styledocco with grunt-styleguide

--- grunt-tasks/options/styleguide.js
+++ grunt-tasks/options/styleguide.js
@@ -1,15 +1,17 @@
-// preprocessor: '/usr/bin/less'
-var config = require('../util/config.js');
-
 module.exports = {
-    config: config,
-    options: {
-        _: ['<%= config.dist %>'],
-        name: 'Encore UI',
-        out: '<%= config.docs %>/styleguide',
-        in : ['<%= config.dist %>'],
-        include: [],
-        preprocessor: 'node_modules/grunt-contrib-less/node_modules/less',
-        basePath: '<%= config.dist %>'
+    styledocco: {
+        options: {
+            framework: {
+                name: 'styledocco',
+                options: {
+                    verbose: true
+                },
+            },
+            name: 'Encore UI'
+        },
+
+        files: {
+            'build/styleguide': 'build/dist/*.css'
+        }
     }
 };
