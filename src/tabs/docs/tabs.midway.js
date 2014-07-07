8e9171636765e54486b94761b0ceea59909ed04f
Kevin Lamping <kevin.lamping@rackspace.com>
2014-07-07 17:24:26 -0500

    feat(tabs): added styles/demo for angular-ui tabs; close #8

---
+++ src/tabs/docs/tabs.midway.js
@@ -0,0 +1,17 @@
+var demoPage = require('../../../utils/demo.page.js');
+var tabsPage = require('../tabs.page.js').tabs;
+var expect = require('chai').use(require('chai-as-promised')).expect;
+
+// Add midway tests to run
+describe('tabs', function () {
+    var tabs;
+
+    before(function () {
+        demoPage.go('#/component/tabs');
+        tabs = tabsPage.initialize($('#tabs'));
+    });
+
+    it('should show element', function () {
+        expect(tabs.isDisplayed()).to.eventually.be.true;
+    });
+});
