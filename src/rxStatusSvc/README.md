cbcfd9aa63c15679a53ff281eb5c8bcebadb7b96
Hussam Dawood <hussam.dawood@rackspace.com>
2014-07-07 15:51:08 -0500

    feat(rxStatusSvc): Wrapper for rxNotify.

---
+++ src/rxStatusSvc/README.md
@@ -0,0 +1,101 @@
+[![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+# Description
+
+This service is provided as a compliment to [`rxNotify` rxNotify service](http://rackerlabs.github.io/encore-ui/#/component/rxNotify).  It abstracts out some of the raw functionality provided by rxNotify to make the addition and removal of single messages easier.
+
+# Usage
+
+## Preparation
+
+In order to use the `Status` service, one has to instantiate it with a proper `$scope` object to keep track of a running state.  `rxNotify` indirectly makes use of the `$scope` variable when a message can be auto-dismissed.  In order to keep the interface for the wrapper functions coherent, the `$scope` variable must be provided before use.  This can be accomplished as follows:
+
+        Status.setupScope($scope);
+
+## Success cases
+
+The `Status` service is provided as a wrapper to rxNotify.  As such, the status types supported by `rxNotify` are still used and have been wrapped into utility functions.  For example, on page load it is usually necessary to inform the user of pending data retrieval.  This can be accomplished by:
+
+        Status.setLoading('Retrieving users');
+
+This will call rxNotify in the following manner:
+
+        rxNotify.add('Retrieving users', {
+            stack: 'page',
+            dismiss: [scope, 'loaded']
+        });
+
+Similarly, the following call using the Status service:
+
+        Status.setSuccess('Successfully deleted questionable ' +
+            'browsing history');
+
+results in a call to rxNotify as such:
+
+        rxNotify.add('Successfully deleted questionable ' +
+            'browsing history',
+            {
+                stack: 'page',
+                show: 'next'
+            }
+        );
+
+Each of the wrapper functions to the different rxNotify message types support receiving an `options:{}` parameter that can override defaults for the respective wrapper. For example, instead of showing a success message on next route change, it can be shown immediately:
+
+        Status.setSuccess('Please show immediately', {
+            show: 'immediate'
+        });
+
+Please note that the `options` are of the same type as one would provide to rxNotify.  This should allow for maximum flexibility when necessary.  However, as a bonus, some common behaviors expected to be overriden have been provided as their own wrapper functions.  For example:
+
+        Status.setSuccessImmediate('Please show immediately')
+
+is the equivalent of calling `Status.setSuccess()` with the `{ show: 'immediate' }` parameter.  Please note, there isn't much fault checking in place, so the following behavior although permitted, is not advised:
+
+        Status.setSuccessImmediate('Please show immediately', {
+            show: 'next'
+        });
+
+## Error cases
+
+The `{ type: 'error' }` wrapper is a unique one.  It allows for a string to be passed as an error message, just like the wrappers before.  For example:
+
+        Status.setError('This is an error!');
+
+
+It also allows for a specialized template to be specified as the error string with an `object:{}` as the second parameter containing the replacements for the template in the error string.  If in a proper format, the object can be automatically parsed using an `ErrorFormatter` and displayed to the user.  For example:
+
+        Status.setError(
+            'Failed loading browsing history: ${message}',
+            { message: 'User has previously cleared their history!' }
+        );
+
+Please note that the replacement variable `${message}` in the error string maps one-to-one to the keys provided in the the error object.  One can specify any number of template variables to replace.  Not providing a balanced list of variables and their replacements will result in a `ReferenceError: <replacement> is not defined`.
+
+The following wrapper functions are available today.  Their names should be self explanatory:
+
+ * setLoading
+ * setSuccess
+ * setSuccessNext
+ * setSuccessImmediate
+ * setWarning
+ * setInfo
+ * setError
+ * complete &rarr; setSuccessImmediate
+
+The following are used to programmatically remove notifications from the screen:
+
+ * dismiss
+ * clear
+
+# Utilities
+
+The `Status` service requires that one provide a `$scope` object to keep tracking of state before any of the wrapper functions can be utilized.  Since it is expected that almost all pages will make use of notifications, one can place the repeated setup of the `Status` service in a page load event handler.  This will allow all pages to gain an already setup `Status` service for immediate use.  For example:
+
+        .run(function ($rootScope, StatusUtil) {
+            $rootScope.$on('$routeChangeSuccess', function () {
+                Status.setupScope($rootScope);
+            });
+        });
+
+Although hidden away in the app's bootstrap code, the above makes for a less repetitive call to `Status.setScope()` at the beginning of each use.
---
+++ src/rxStatusSvc/rxStatusSvc.js
@@ -0,0 +1,193 @@
+angular.module('encore.ui.rxStatusSvc', ['encore.ui.rxNotify'])
+/**
+* @ngdoc service
+* @name encore.ui.rxStatusSvc:StatusUtil
+* @description
+* Manipulates references to needed $scope input for proper notification functionality
+*
+* @example
+* <pre>
+*   $rootScope.$on('$routeChangeSuccess', function () {
+*       Status.setScope(); // no input results in $rootScope being used
+*       Status.setUtil($rootScope); // forcibly set $rootScope as the scope to be used
+*   });
+* </pre>
+*/
+    .service('StatusUtil', function ($route, $rootScope, Status) {
+        return {
+            setupScope: function (scope) {
+                Status.setScope(scope || $rootScope);
+            }
+        };
+    })
+/**
+* @ngdoc service
+* @name encore.ui.rxStatusSvc:Status
+* @description
+* Manages notifications for rxNotify with an abstracted set of functions for ease of use
+*/
+    .service('Status', function ($rootScope, rxNotify, ErrorFormatter) {
+        var stack = 'page';
+        var scope;
+        var status = {
+            LOADING: function () {
+                return { loaded: false, loading: true, prop: 'loaded' };
+            },
+            SUCCESS: function () {
+                return { loaded: true, loading: false, success: true, type: 'success', prop: 'loaded' };
+            },
+            ERROR: function () {
+                return { loaded: true, loading: false, success: false, type: 'error', prop: 'loaded' };
+            },
+            WARNING: function () {
+                return { loaded: true, loading: false, success: true, type: 'warning', prop: 'loaded' };
+            },
+            INFO: function () {
+                return { loaded: true, loading: false, success: true, type: 'info', prop: 'loaded' };
+            },
+            CLEAR: function () {
+                return { loading: false, prop: 'loaded' };
+            },
+        };
+
+        // States that specify a type cannot be dismissed (have to be approved by user)
+        var isDismissable = function (state) {
+            return _.has(state, 'loading') && !_.has(state, 'type');
+        };
+
+        // Given an options object, check if scope[options.prop] exists,
+        // and set it to `val` if so. `val` defaults to true if not
+        // supplied
+        var setDoneLoadingProp = function (options, val) {
+            val = _.isUndefined(val) ? true : val;
+            if (_.has(options, 'prop') && _.has(scope, options.prop)) {
+                scope[options.prop] = val;
+            }
+        };
+
+        // If the stack is overridden in a given controller, it needs to be refreshed
+        // for any subsequent controllers since a Service is loaded by Angular only once
+        $rootScope.$on('$routeChangeStart', function () {
+            status.setStack('page');
+        });
+
+        status.setStack = function (s) {
+            stack = s;
+        };
+
+        status.setScope = function ($scope) {
+            scope = $scope;
+            scope.loaded = false;
+        };
+
+        status.setStatus = function (msg, state) {
+            state.stack = stack;
+
+            if (isDismissable(state)) {
+                // state.prop defaults to 'loaded', per status.LOADING
+                state.dismiss = [scope, state.prop];
+            }
+
+            if (state.type === 'success') {
+                state.show = state.show || 'next';
+            }
+
+            setDoneLoadingProp(state, _.has(state, 'loading') ? !state.loading : true);
+            scope.status = state;
+            return rxNotify.add(msg, state);
+        };
+
+        status.setLoading = function (msg, options) {
+            options = _.isUndefined(options) || _.isEmpty(options) ? {} : options;
+            options = _.merge(status.LOADING(), options);
+
+            // prop is the variable on scope that stores whether this loading is complete
+            // By default is uses $scope.loaded, but individual messages should be able to
+            // use their own property
+            var prop = options.prop;
+            scope[prop] = false;
+            return status.setStatus(msg || '', options);
+        };
+
+        status.setSuccess = function (msg, options) {
+            options = _.isUndefined(options) || _.isEmpty(options) ? {} : options;
+            options = _.merge(status.SUCCESS(), options);
+            return status.setStatus(msg || '', options);
+        };
+
+        status.setSuccessNext = function (msg, options) {
+            var next = { 'show': 'next' };
+            options = _.merge(next, options);
+            return status.setSuccess(msg, options);
+        };
+
+        status.setSuccessImmediate = function (msg, options) {
+            var immediate = { 'show': 'immediate' };
+            options = _.merge(immediate, options);
+            return status.setSuccess(msg, options);
+        };
+
+        status.setWarning = function (msg, options) {
+            options = _.merge(status.WARNING(), options);
+            return status.setStatus(msg, options);
+        };
+
+        status.setInfo = function (msg, options) {
+            options = _.merge(status.INFO(), options);
+            return status.setStatus(msg, options);
+        };
+
+        /*
+         * `msg` - can be a plain string, or it can be a string template with ${message} in it
+         * `error` - An optional error object. Should have a `message` or `statusText` property
+         * `options` - A usual options object
+         */
+        status.setError = function (msg, error, options) {
+            options = _.isUndefined(options) || _.isEmpty(options) ? {} : options;
+            options = _.merge(status.ERROR(), options);
+            msg = ErrorFormatter.buildErrorMsg(msg || '', error);
+            return status.setStatus(msg, options);
+        };
+
+        status.complete = function (options) {
+            options = options || {};
+            return status.setSuccessImmediate('', _.merge(status.SUCCESS(), options));
+        };
+
+        status.dismiss = function (obj) {
+            scope.status = status.CLEAR();
+            return rxNotify.dismiss(obj);
+        };
+
+        status.clear = function (st) {
+            scope.status = status.CLEAR();
+            return rxNotify.clear(st || stack);
+        };
+
+        return status;
+    })
+/**
+* @ngdoc service 
+* @name encore.ui.rxStatusSvc:ErrorFormatter
+* @description
+* Provides a helper method to parse error objects for 'message' and format them
+* as necessary for Status.setError()
+*/
+    .factory('ErrorFormatter', function () {
+        /*
+         * formatString is a string with ${message} in it somewhere, where ${message}
+         * will come from the `error` object. The `error` object either needs to have
+         * a `message` property, or a `statusText` property.
+         */
+        var buildErrorMsg = function (formatString, error) {
+            error = error || {};
+            if (!_.has(error, 'message')) {
+                error.message = _.has(error, 'statusText') ? error.statusText : 'Unknown error';
+            }
+            return _.template(formatString, error);
+        };
+
+        return {
+            buildErrorMsg: buildErrorMsg
+        };
+    });
