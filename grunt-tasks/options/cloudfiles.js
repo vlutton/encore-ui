13d4585af0eae956062436038a979d83754a4803
Kevin Lamping <kevin.lamping@rackspace.com>
2014-07-31 15:49:11 -0500

    feat(travis): post wraith results to CDN

--- grunt-tasks/options/cloudfiles.js
+++ grunt-tasks/options/cloudfiles.js
@@ -1,3 +1,5 @@
+/*jshint node:true*/
+
 module.exports = {
     alpha: {
         'user': '<%= localConfig.cloudUsername %>',
@@ -38,6 +40,17 @@ module.exports = {
             ],
             'dest': '/',
             // Remove '/dist/build/' from path
+            'stripcomponents': 2
+        }]
+    },
+    wraith: {
+        'user': 'encorecloudfiles',
+        'key': process.env.cloudFilesApi,
+        'region': 'IAD',
+        'upload': [{
+            'container': 'encore-ui-wraith',
+            'src': '<%= config.wraith %>/shots/**/*',
+            'dest': '/<%= grunt.option("pr") %>/',
             'stripcomponents': 2
         }]
     }
