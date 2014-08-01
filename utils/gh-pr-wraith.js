a23b9ef69de9fd348000a96cd1985d0d7dcb4511
Kevin Lamping <kevin.lamping@rackspace.com>
2014-08-01 11:48:01 -0500

    fix(travis): clean up gh-pr-wraith

--- utils/gh-pr-wraith.js
+++ utils/gh-pr-wraith.js
@@ -3,7 +3,10 @@ var https = require('https');
 
 var prNumber = process.argv[2];
 var cdnUrl = 'http://eb4d4842ea70055541c8-b6a3ba129b5eb76af13f4d17e4c1a53b.r51.cf5.rackcdn.com/';
-var comment = '[Wraith UI diff](' + cdnUrl + prNumber + '/gallery.html)';
+
+var data = {
+    'body': '[Wraith UI diff](' + cdnUrl + prNumber + '/gallery.html)'
+};
 
 var options = {
     hostname: 'api.github.com',
@@ -11,22 +14,23 @@ var options = {
     method: 'POST',
     headers: {
         'User-Agent': 'Encore-Wraith-Script',
+        'Content-Type': 'application/json',
         'Authorization': 'token ' + process.env.ghToken
     }
 };
 
 var req = https.request(options, function (res) {
-    process.stdout.write('statusCode: ', res.statusCode);
-    process.stdout.write('headers: ', res.headers);
-
-    res.on('data', function (d) {
-        process.stdout.write(d);
-    });
+    if (res.statusCode !== 201) {
+        console.error('statusCode: ', res.statusCode);
+        console.error('headers: ', res.headers);
+        process.exit(1);
+    }
 });
 
-req.write('{ "body": "' + comment + '"}');
-req.end();
+req.write(JSON.stringify(data));
 
 req.on('error', function (e) {
     console.error(e);
 });
+
+req.end();
