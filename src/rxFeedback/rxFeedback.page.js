4835dec5cfe665e49032a3c15fadc38ad7094e36
Droogans <droogans@gmail.com>
2014-09-02 15:45:30 -0500

    test(rxFeedback): Page objects and tests.
    Closes #323

--- src/rxFeedback/rxFeedback.page.js
+++ src/rxFeedback/rxFeedback.page.js
@@ -1,11 +1,109 @@
 /*jshint node:true*/
-var Page = require('astrolabe').Page;
 
 var rxFeedback = {
 
-    isDisplayed: {
+    selReportType: {
+        get: function () {
+            return this.rootElement.element(by.model('fields.type'));
+        }
+    },
+
+    txtFeedback: {
+        get: function () {
+            return this.rootElement.element(by.model('fields.description'));
+        }
+    },
+
+    open: {
         value: function () {
-            return this.rootElement.isDisplayed();
+            var page = this;
+            return this.isDisplayed().then(function (isDisplayed) {
+                if (!isDisplayed) {
+                    page.eleFeedback.$('a').click();
+                }
+            });
+        }
+    },
+
+    type: {
+        get: function () {
+            return this.selReportType.$('option:checked').getText();
+        },
+        set: function (optionText) {
+            var option = this.selReportType.element(by.cssContainingText('option', optionText));
+            browser.actions().mouseDown(option).mouseUp().perform();
+        }
+    },
+
+    types: {
+        get: function () {
+            return this.selReportType.$$('option').map(function (option) {
+                return option.getText();
+            });
+        }
+    },
+
+    description: {
+        get: function () {
+            return this.txtFeedback.getAttribute('value');
+        },
+        set: function (feedback) {
+            this.txtFeedback.clear();
+            this.txtFeedback.sendKeys(feedback);
+        }
+    },
+
+    descriptionPlaceholder: {
+        get: function () {
+            return this.txtFeedback.getAttribute('placeholder');
+        }
+    },
+
+    descriptionLabel: {
+        get: function () {
+            return this.rootElement.$('.feedback-description').getText();
+        }
+    },
+
+    send: {
+        // Prepares, writes, and submits feedback.
+        // If `confirmSuccessWithin` is defined, a confirmation of submission success must appear
+        // within `confirmSuccessWithin` milliseconds.
+        //
+        // If confirmSuccessFn is undefined, the default behavior will look for an rxNotify success
+        // message. Otherwise, `confirmSuccessFn` will be attempted until it yields a truthy value,
+        // using Protractor's `wait` function.
+        value: function (feedbackType, feedbackText, confirmSuccessWithin, confirmSuccessFn) {
+            var page = this;
+            return this.isDisplayed().then(function (isDisplayed) {
+                if (!isDisplayed) {
+                    page.open();
+                }
+                page.reportType = feedbackType;
+                page.description = feedbackText;
+                page.submit();
+                if (confirmSuccessWithin !== undefined) {
+                    page.confirmSuccess(confirmSuccessWithin, confirmSuccessFn);
+                }
+
+            });
+        }
+    },
+
+    confirmSuccess: {
+        value: function (within, fn) {
+            if (fn === undefined) {
+                var notifications = exports.rxNotify || require('../rxNotify/rxNotify.page.js').rxNotify;
+                fn = function () {
+                    return notifications.all.byType('success').then(function (successes) {
+                        return successes.length > 0;
+                    });
+                };
+            }
+
+            browser.wait(function () {
+                return fn();
+            }, within, 'feedback submission did not confirm success within ' + within + ' msecs');
         }
     }
 
@@ -14,15 +112,11 @@ var rxFeedback = {
 exports.rxFeedback = {
 
     initialize: function (rxFeedbackElement) {
-        rxFeedback.rootElement = {
+        var modal = exports.rxModalAction || require('../rxModalAction/rxModalAction.page.js').rxModalAction;
+        rxFeedback.eleFeedback = {
             get: function () { return rxFeedbackElement; }
         };
-        return Page.create(rxFeedback);
-    },
-
-    // Displays a helpful, console.log-able version of the page object.
-    // Astrolabe's objects will not contain the methods available.
-    // Use this method instead to see what properties and functions exist.
-    help: function () { return Object.keys(rxFeedback); }
+        return modal.initialize(rxFeedback);
+    }
 
 };
