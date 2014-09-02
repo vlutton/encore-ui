86b7d0fe8471c33b60573d201e5fdf7065b19488
Droogans <droogans@gmail.com>
2014-09-02 10:39:54 -0500

    test(rxModalAction): Page objects, tests.
    Closes #328

--- src/rxModalAction/docs/rxModalAction.midway.js
+++ src/rxModalAction/docs/rxModalAction.midway.js
@@ -1,16 +1,78 @@
 var demoPage = require('../../../utils/demo.page.js');
-var rxModalActionPage = require('../rxModalAction.page.js').rxModalAction;
+var modal = require('../rxModalAction.page.js').rxModalAction;
 
 // Add midway tests to run
 describe('rxModalAction', function () {
-    var ptor = rxModalActionPage.driver;
+    var changePasswordModal, triggerModal;
 
-    it('beforeAll', function () {
+    before(function () {
         demoPage.go('#/component/rxModalAction');
+        triggerModal = function () {
+            $('.component-demo .modal-link').click();
+        };
+
+        var customFunctionalty = {
+            txtNewPassword: {
+                get: function () {
+                    return this.rootElement.element(by.model('fields.password'));
+                }
+            },
+
+            newPassword: {
+                get: function () {
+                    return this.txtNewPassword.getAttribute('value');
+                },
+                set: function (password) {
+                    this.txtNewPassword.clear();
+                    this.txtNewPassword.sendKeys(password);
+                }
+            }
+        };
+
+        changePasswordModal = modal.initialize(customFunctionalty);
     });
 
-    // it('should show element', function () {
-    //     // will fail b/c there is no element being added in component.html
-    //     expect(rxModalActionPage.rxModalActionElement.isDisplayed()).toEqual(true);
-    // });
+    it('should not display the modal unless triggered', function () {
+        expect(changePasswordModal.isDisplayed()).to.eventually.be.false;
+    });
+
+    it('should display the modal when triggered', function () {
+        triggerModal();
+        expect(changePasswordModal.isDisplayed()).to.eventually.be.true;
+    });
+
+    it('should close the modal using the little X in the corner', function () {
+        changePasswordModal.close();
+        expect(changePasswordModal.isDisplayed()).to.eventually.be.false;
+    });
+
+    it('should close the modal when clicking the "cancel" button', function () {
+        triggerModal();
+        changePasswordModal.cancel();
+        expect(changePasswordModal.isDisplayed()).to.eventually.be.false;
+    });
+
+    it('should have a title', function () {
+        triggerModal();
+        expect(changePasswordModal.title).to.eventually.equal('Change hey_dude Admin Password');
+    });
+
+    it('should have a subtitle', function () {
+        expect(changePasswordModal.subtitle).to.eventually.equal('Please read instructions below');
+    });
+
+    it('should not let me submit the modal by default', function () {
+        expect(changePasswordModal.canSubmit()).to.eventually.be.false;
+    });
+
+    it('should let me submit the modal when a new password is entered', function () {
+        changePasswordModal.newPassword = 'hunter2';
+        expect(changePasswordModal.canSubmit()).to.eventually.be.true;
+    });
+
+    it('should submit', function () {
+        changePasswordModal.submit();
+        expect(changePasswordModal.isDisplayed()).to.eventually.be.false;
+    });
+
 });
--- src/rxModalAction/rxModalAction.page.js
+++ src/rxModalAction/rxModalAction.page.js
@@ -1,11 +1,71 @@
 /*jshint node:true*/
 var Page = require('astrolabe').Page;
+var _ = require('lodash');
 
-exports.rxModalAction = Page.create({
-    // Elements
-    rxModalActionElement: {
+var rxModalAction = {
+
+    rootElement: {
         get: function () {
-            return element(by.id('rxModalActionElement'));
+            return $('.modal');
+        }
+    },
+
+    btnSubmit: {
+        get: function () {
+            return this.rootElement.$('.submit');
+        }
+    },
+
+    isDisplayed: {
+        value: function () {
+            return this.rootElement.isPresent();
+        }
+    },
+
+    title: {
+        get: function () {
+            return this.rootElement.$('.modal-title').getText();
+        }
+    },
+
+    subtitle: {
+        get: function () {
+            return this.rootElement.$('.modal-subtitle').getText();
+        }
+    },
+
+    close: {
+        value: function () {
+            this.rootElement.$('.modal-close').click();
+        }
+    },
+
+    canSubmit: {
+        value: function () {
+            return this.btnSubmit.getAttribute('disabled').then(function (isDisabled) {
+                return isDisabled === null;
+            });
+        }
+    },
+
+    submit: {
+        value: function () {
+            this.btnSubmit.click();
+        }
+    },
+
+    cancel: {
+        value: function () {
+            this.rootElement.$('.cancel').click();
         }
     }
-});
+
+};
+
+exports.rxModalAction = {
+
+    initialize: function (customFunctionality) {
+        return Page.create(_.merge(customFunctionality, rxModalAction));
+    }
+
+};
