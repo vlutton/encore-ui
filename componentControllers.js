ddd062f423a215723609363d94842fefffc699e9
Jay Parlar <parlar@gmail.com>
2014-09-15 16:54:22 -0400

    docs(ghpages): release v1.1.5

--- componentControllers.js
+++ componentControllers.js
@@ -39,6 +39,26 @@ function hotkeysCtrl ($scope, hotkeys) {
     });
 }
 
+/*jshint unused:false*/
+function rxActionMenuCtrl ($scope, rxNotify) {
+
+    $scope.add = function () {
+        rxNotify.add('Added!', {
+            type: 'success',
+            repeat: false,
+            timeout: 3
+        });
+    };
+
+    $scope.remove = function () {
+        rxNotify.add('Deleted!', {
+            type: 'error',
+            repeat: false,
+            timeout: 3
+        });
+    };
+
+}
 
 
 
