8948163e2c6e07233f724b75211e51de6913c814
Jay Parlar <parlar@gmail.com>
2014-11-04 13:57:42 -0500

    feat(testing): Add a debug mode for testing
    If you want to use the `debugger` statement to pause execution
    during your tests, you need to have karma started in a particular way.
    This lets you do `grunt test:debug`, which will cause the tests to be
    run in Chrome, and will keep the browser open.
    
    After Chrome is launched, you must manually open Chrome's web inspector
    before the `debugger` breakpoints will be respected

--- grunt-tasks/test.js
+++ grunt-tasks/test.js
@@ -6,10 +6,14 @@ module.exports = function (grunt) {
     // I could have a `server:testing` task run beforehand to give us the server,
     // but this gets in the way of our Travis configuration
     grunt.registerTask('test', 'Run tests', function (mode) {
-        grunt.task.run('karma:single');
-        if (mode === 'full') {
-            grunt.task.run('modules');
-            grunt.task.run('phantomjs-check:allModules');
+        if (mode === 'debug') {
+            grunt.task.run('karma:debug');
+        } else {
+            grunt.task.run('karma:single');
+            if (mode === 'full') {
+                grunt.task.run('modules');
+                grunt.task.run('phantomjs-check:allModules');
+            }
         }
     });
 };
