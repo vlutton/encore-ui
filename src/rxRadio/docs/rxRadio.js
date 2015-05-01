ba2e32dc1203cc0460a63a98c3f6089ee722294c
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-05-01 17:53:15 -0500

    feat(rxRadio): add rxRadio component
    Per necessity of rxForm rework, but this directive can stand alone
    for use elsewhere in the framework.
    
    * automatically sets `type="radio"`
    * `$destroy` support
    * apply `rx-disabled` class to wrapper if disabled by either `disabled` or `ng-disabled`
    * exercises added to test `ng-disabled`, `disabled`, and `type="radio"` functionality

---
+++ src/rxRadio/docs/rxRadio.js
@@ -0,0 +1,9 @@
+/*jshint unused:false*/
+angular.module('demoApp')
+.controller('rxRadioCtrl', function ($scope) {
+    $scope.ngDisabledRadio = 1;
+    $scope.disabledRadio = 1;
+    $scope.enabledRadio = 1;
+    $scope.errRadio = 1;
+    $scope.radCreateDestroy = 'destroyed';
+});
