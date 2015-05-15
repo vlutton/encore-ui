809d9bf4aded4f13cdef9bd095b9b90a59b5a2ce
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-05-15 16:29:10 -0500

    fix(componentTemplate): update to angular 1.3+ controller syntax

--- grunt-tasks/component-template/root/docs/component.js
+++ grunt-tasks/component-template/root/docs/component.js
@@ -1,6 +1,6 @@
 /*jshint unused:false*/
-
-// This file is used to help build the 'demo' documentation page and should be updated with example code
-function {%= name %}Ctrl ($scope) {
-
-}
+angular.module('demoApp')
+.controller('{%= name %}Ctrl', function ($scope) {
+    // This controller is used to help build the 'demo' documentation page
+    // and should be updated with example code
+});
