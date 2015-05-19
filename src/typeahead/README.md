75ed17ae58a6630b23295d42013ffd1a510d4d28
megalord <jordan.griege@rackspace.com>
2015-05-19 13:43:38 -0500

    feat(typeahead): show options when focused

--- src/typeahead/README.md
+++ src/typeahead/README.md
@@ -5,3 +5,5 @@ This component provides styles and a demo for the [the Angular-UI Bootstrap Type
 ## Usage
 
 Usage is the exact same as demoed on the Angular-UI Bootstrap site. See [the Angular-UI Bootstrap Docs](http://angular-ui.github.io/bootstrap/) for further guidance on usage and configuration of this component.
+
+A feature has been added that shows the list of options when the input receives focus.  This list is still filtered according to the input's value, except when the input is empty.  In that case, all the options are shown.  To use this feature, add the `allowEmpty` parameter to the `filter` filter in the `typeahead` attribute.  See the demo for an example.
---
+++ src/typeahead/typeahead.js
@@ -0,0 +1,46 @@
+angular.module('encore.ui.typeahead', ['ui.bootstrap'])
+.config(function ($provide) {
+    $provide.decorator('typeaheadDirective', function ($delegate, $filter) {
+        var typeahead = $delegate[0];
+        var link = typeahead.link;
+        var lowercase = $filter('lowercase');
+
+        typeahead.compile = function () {
+            return function (scope, element, attrs, ngModelCtrl) {
+                link.apply(this, arguments);
+
+                if (/allowEmpty/.test(attrs.typeahead)) {
+                    var EMPTY_KEY = '$EMPTY$';
+
+                    // Wrap the directive's $parser such that the $viewValue
+                    // is not empty when the function runs.
+                    ngModelCtrl.$parsers.unshift(function ($viewValue) {
+                        var value = _.isEmpty($viewValue) ? EMPTY_KEY : $viewValue;
+                        // The directive will check this equality before populating the menu.
+                        ngModelCtrl.$viewValue = value;
+                        return value;
+                    });
+
+                    ngModelCtrl.$parsers.push(function ($viewValue) {
+                        return $viewValue === EMPTY_KEY ? '' : $viewValue;
+                    });
+
+                    element.on('click', function () {
+                        scope.$apply(function () {
+                            ngModelCtrl.$setViewValue(ngModelCtrl.$viewValue);
+                        });
+                    });
+
+                    scope.allowEmpty = function (actual, expected) {
+                        if (expected === EMPTY_KEY) {
+                            return true;
+                        }
+                        return lowercase(actual).indexOf(lowercase(expected)) !== -1;
+                    };
+                }
+            };
+        };
+
+        return $delegate;
+    });
+});
