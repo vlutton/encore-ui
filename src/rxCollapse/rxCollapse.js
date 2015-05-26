3f4b964b8b277b1b59afe3eae775529ea4ddaa2a
Chris Carter <chris@ccarteronline.com>
2015-05-26 17:53:36 -0500

    feat(rxCollapse): if collapsed in 2nd version and comment

--- src/rxCollapse/rxCollapse.js
+++ src/rxCollapse/rxCollapse.js
@@ -28,14 +28,25 @@ angular.module('encore.ui.rxCollapse', [])
             scope.storedTitles = ['See Less', 'See More'];
             scope.isExpanded = (attrs.expanded === 'false') ? false : true;
 
-            
-            scope.doSeeMoreOrLess = function () {
-                
-                scope.toggleTitle = scope.storedTitles[0];
+            //When the see more/less version is chosed, 
+            //take the value and togle the title accordingly
+            //either way, hide the border and hide the default style
+            scope.doSeeMoreOrLess = function (expVal) {
+                if (expVal) {
+                    scope.toggleTitle = scope.storedTitles[0];
+                    
+                } else {
+                    scope.toggleTitle = scope.storedTitles[1];
+                }
+
                 scope.cContainer = 'hideBorder';
                 scope.tabToShow = true;
+                
             };
 
+            //When a user clicks on see more/less, toggle the
+            //boolean statement involving expansion and toggle
+            //the title accordingly
             scope.toggleTitleAndCollapse = function () {
 
                 scope.isExpanded = !scope.isExpanded;
@@ -47,8 +58,10 @@ angular.module('encore.ui.rxCollapse', [])
                 }
             };
 
+            //Check if the title exists. If it does not, show other version
+            //if it does exist, hide the markup for the previous version
             if (!scope.title) {
-                scope.doSeeMoreOrLess();
+                scope.doSeeMoreOrLess(scope.isExpanded);
                   
             } else {
                 scope.tabToShow = false;
