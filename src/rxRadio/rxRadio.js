a546cc7a0e4bb35cf9ffcfed2a5badaadb08d61b
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-05-28 15:09:58 -0500

    fix(ng1dot3): modify for angular 1.3 support

--- src/rxRadio/rxRadio.js
+++ src/rxRadio/rxRadio.js
@@ -12,32 +12,34 @@ angular.module('encore.ui.rxRadio', [])
 
             return function (scope, element, attrs) {
                 var disabledClass = 'rx-disabled';
-                var wrapper = angular.element('<div class="rxRadio"></div>');
+                var wrapper = '<div class="rxRadio"></div>';
                 var fakeRadio = '<div class="fake-radio">' +
                         '<div class="tick"></div>' +
                     '</div>';
 
                 element.wrap(wrapper);
                 element.after(fakeRadio);
+                // must be defined AFTER the element is wrapped
+                var parent = element.parent();
 
                 // apply/remove disabled attribute so we can
                 // apply a CSS selector to style sibling elements
                 if (attrs.disabled) {
-                    wrapper.addClass(disabledClass);
+                    parent.addClass(disabledClass);
                 }
                 if (_.has(attrs, 'ngDisabled')) {
                     scope.$watch('ngDisabled', function (newVal) {
                         if (newVal === true) {
-                            wrapper.addClass(disabledClass);
+                            parent.addClass(disabledClass);
                         } else {
-                            wrapper.removeClass(disabledClass);
+                            parent.removeClass(disabledClass);
                         }
                     });
                 }
 
                 // remove stylistic markup when element is destroyed
                 element.on('$destroy', function () {
-                    wrapper[0].remove();
+                    parent[0].remove();
                 });
             };
         }//compile
