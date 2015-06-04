1bcfe33fe7bf0806171300d31c118ac9676f109f
Chris Carter <chris@ccarteronline.com>
2015-06-04 16:58:49 -0500

    style(rxCollapse): rxCollapseSeeMoreorLess
    Cleaned up some spaces, removed un-used items.

--- src/rxCollapse/rxCollapse.js
+++ src/rxCollapse/rxCollapse.js
@@ -7,12 +7,13 @@ angular.module('encore.ui.rxCollapse', [])
  * @description
  * Hide and show an element with a transition.
  *
- * @param {string} [title] The title to display next to the toggle button.
+ * @param {string} [title] The title to display next to the toggle button and toggle different rxCollapse
  * @param {string} [expanded] Initially expanded or collapsed.  Default is expanded.
  *
  * @example
  * <pre>
- *     <rx-collapse title="Filter results" expanded="true"></rx-collapse>
+ *     <rx-collapse title="Filter results" expanded="true">Text Here</rx-collapse>
+ *     <rx-collapse expanded="true">Text Here</rx-collapse>
  * </pre>
  */
 .directive('rxCollapse', function () {
@@ -28,29 +29,18 @@ angular.module('encore.ui.rxCollapse', [])
             scope.storedTitles = ['See Less', 'See More'];
             scope.isExpanded = (attrs.expanded === 'false') ? false : true;
 
-            //When the see more/less version is chosed,
-            //take the value and togle the title accordingly
-            //either way, hide the border and hide the default style
             scope.doSeeMoreOrLess = function (expVal) {
                 if (expVal) {
                     scope.toggleTitle = scope.storedTitles[0];
-
                 } else {
                     scope.toggleTitle = scope.storedTitles[1];
                 }
-
                 scope.cContainer = 'hideBorder';
                 scope.tabToShow = true;
-
             };
 
-            //When a user clicks on see more/less, toggle the
-            //boolean statement involving expansion and toggle
-            //the title accordingly
             scope.toggleTitleAndCollapse = function () {
-
                 scope.isExpanded = !scope.isExpanded;
-
                 if (scope.isExpanded) {
                     scope.toggleTitle = scope.storedTitles[0];
                 } else {
@@ -67,15 +57,11 @@ angular.module('encore.ui.rxCollapse', [])
                 }
             };
 
-            //Check if the title exists. If it does not, show other version
-            //if it does exist, hide the markup for the previous version
             if (!scope.title) {
                 scope.doSeeMoreOrLess(scope.isExpanded);
-
             } else {
                 scope.tabToShow = false;
             }
-
             scope.setChev(scope.isExpanded);
         }
     };
