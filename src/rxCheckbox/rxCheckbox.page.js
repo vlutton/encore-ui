c51162e04daeae3078e4de557c7f582910af4861
Droogans <droogans@gmail.com>
2015-07-09 19:01:19 +0300

    test(rxCheckbox): Refactor page objects.
    Since the htmlCheckbox and rxCheckbox objects are so similar, it's
    easier to simply use one all of the time. In this case, it's
    rxCheckbox.
    
    BREAKING CHANGE: All instances of `encore.htmlCheckbox` in your end to end
    tests will need to moved to `encore.rxCheckbox`.

--- src/rxCheckbox/rxCheckbox.page.js
+++ src/rxCheckbox/rxCheckbox.page.js
@@ -5,14 +5,74 @@ var Page = require('astrolabe').Page;
 /**
  * @namespace
  */
-var htmlCheckbox = {
+var rxCheckbox = {
+    eleWrapper: {
+        get: function () {
+            return this.rootElement.element(by.xpath('..'));
+        }
+    },
+
+    eleFakeCheckbox: {
+        get: function () {
+            return this.eleWrapper.$('.fake-checkbox');
+        }
+    },
+
+    /**
+       Useful for situations where input types might change in the future, ensuring that the expected one is being used.
+       @returns {Boolean} Whether or not the element in question is a checkbox.
+     */
+    isCheckbox: {
+        value: function () {
+            return this.rootElement.getAttribute('type').then(function (type) {
+                return type === 'checkbox';
+            });
+        }
+    },
+
     /**
      * @function
      * @returns {Boolean} Whether the checkbox is currently displayed.
      */
     isDisplayed: {
         value: function () {
-            return this.rootElement.isDisplayed();
+            var page = this;
+            return this.eleFakeCheckbox.isPresent().then(function (isFakeCheckbox) {
+                return isFakeCheckbox ? page.eleFakeCheckbox.isDisplayed() : page.rootElement.isDisplayed();
+            });
+        }
+    },
+
+    /**
+     * @function
+     * @returns {Boolean} Whether or not the checkbox is disabled.
+     */
+    isDisabled: {
+        value: function () {
+            var page = this;
+            return this.eleFakeCheckbox.isPresent().then(function (isFakeCheckbox) {
+                if (isFakeCheckbox) {
+                    return page.eleWrapper.getAttribute('class').then(function (classes) {
+                        return _.contains(classes.split(' '), 'rx-disabled');
+                    });
+                }
+                return page.rootElement.getAttribute('disabled').then(function (disabled) {
+                    return (disabled ? true : false);
+                });
+            });
+        }
+    },
+
+    /**
+     * @function
+     * @returns {Boolean} Whether or not the checkbox is present on the page.
+     */
+    isPresent: {
+        value: function () {
+            var page = this;
+            return this.eleFakeCheckbox.isPresent().then(function (isFakeCheckbox) {
+                return isFakeCheckbox || page.rootElement.isPresent();
+            });
         }
     },
 
@@ -29,34 +89,12 @@ var htmlCheckbox = {
     },
 
     /**
-     * @function
-     * @returns {Boolean} Whether or not the checkbox is currently selected
+       @function
+       @returns {Boolean} Whether or not the checkbox is selected.
      */
     isSelected: {
         value: function () {
             return this.rootElement.isSelected();
-        }
-    },
-
-    /**
-     * @function
-     * @returns {Boolean} Whether or not the checkbox is disabled
-     */
-    isDisabled: {
-        value: function () {
-            return this.rootElement.getAttribute('disabled').then(function (disabled) {
-                return (disabled ? true : false);
-            });
-        }
-    },
-
-    /**
-     * @function
-     * @returns {Boolean} Whether or not the checkbox exists on the page
-     */
-    isPresent: {
-        value: function () {
-            return this.rootElement.isPresent();
         }
     },
 
@@ -79,7 +117,7 @@ var htmlCheckbox = {
      * @deprecated
      * @function
      * @description
-     * **DEPRECATED**: Use {@link htmlCheckbox.deselect} instead.
+     * **DEPRECATED**: Use {@link rxCheckbox.deselect} instead.
      * This function will be removed in a future release of the EncoreUI framework.
      * @returns {undefined}
      */
@@ -106,118 +144,6 @@ var htmlCheckbox = {
 };
 
 /**
- * @namespace
- * @extends htmlCheckbox
- */
-var rxCheckbox = {
-    eleWrapper: {
-        get: function () {
-            return this.rootElement.element(by.xpath('..'));
-        }
-    },
-
-    eleFakeCheckbox: {
-        get: function () {
-            return this.eleWrapper.$('.fake-checkbox');
-        }
-    },
-
-    /**
-     * @function
-     * @returns {Boolean} Whether root element is of type="checkbox"
-     */
-    isCheckbox: {
-        value: function () {
-            return this.rootElement.getAttribute('type').then(function (ilk) {
-                return ilk === 'checkbox';
-            });
-        }
-    },
-
-    /**
-     * @function
-     * @returns {Boolean} Whether the fake checkbox is currently displayed.
-     */
-    isDisplayed: {
-        value: function () {
-            return this.eleFakeCheckbox.isDisplayed();
-        }
-    },
-
-    /**
-     * @function
-     * @returns {Boolean} Whether or not the wrapper has expected disabled class name
-     */
-    isDisabled: {
-        value: function () {
-            return this.eleWrapper.getAttribute('class').then(function (classes) {
-                return _.contains(classes.split(' '), 'rx-disabled');
-            });
-        }
-    },
-
-    /**
-     * @function
-     * @returns {Boolean} Whether or not the styled element exists on the page
-     */
-    isPresent: {
-        value: function () {
-            return this.eleFakeCheckbox.isPresent();
-        }
-    }
-};
-rxCheckbox = _.assign(htmlCheckbox, rxCheckbox);
-
-/**
- * @exports encore.htmlCheckbox
- * @description Page object for interacting with plain old html checkboxes
- */
-exports.htmlCheckbox = {
-    /**
-     * @function
-     * @param {WebElement} checkboxElement
-     *   WebElement to be transformed into an htmlCheckbox page object.
-     * @returns {htmlCheckbox} Page object representing the checkbox object
-     */
-    initialize: function (checkboxElement) {
-        htmlCheckbox.rootElement = {
-            get: function () { return checkboxElement; }
-        };
-        return Page.create(htmlCheckbox);
-    },
-
-    /**
-     * @returns {htmlCheckbox}
-     *   Page object representing the _first_ `<input type="checkbox" />`
-     *   object found on the page.
-     */
-    main: (function () {
-        htmlCheckbox.rootElement = {
-            get: function () { return $('input[type="checkbox"]'); }
-        };
-        return Page.create(htmlCheckbox);
-    })(),
-
-    /**
-     * @function
-     * @description Generates a getter and a setter for a checkbox element.
-     * @param {WebElement} elem - The WebElement for the checkbox.
-     * @returns {Object} A getter and a setter to be applied to a checkbox in a page object.
-     */
-    generateAccessor: function (elem) {
-        return {
-            get: function () {
-                return exports.htmlCheckbox.initialize(elem).isSelected();
-            },
-            set: function (enable) {
-                var checkbox = exports.htmlCheckbox.initialize(elem);
-                enable ? checkbox.select() : checkbox.deselect();
-            }
-        };
-    }
-};
-
-/**
  * @exports encore.rxCheckbox
  * @description Page object for interacting with rxCheckbox elements
  */
@@ -235,7 +161,9 @@ exports.rxCheckbox = {
     },
 
     /**
-     * @returns {rxCheckbox} Page object representing the _first_ `<input rx-checkbox />` element found on the page
+       Don't use this if you're expecting a regular html checkbox on the page. This only checks for Encore
+       specific rxForm-style checkboxes.
+       @returns {rxCheckbox} Page object representing the _first_ `<input rx-checkbox />` element found on the page.
      */
     main: (function () {
         rxCheckbox.rootElement = {
