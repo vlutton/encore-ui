444834b7e4c8f3dbdc345ec4f7eb5e456e228e1f
Droogans <droogans@gmail.com>
2015-07-20 20:52:59 +0300

    test(rxMisc): Unless function

--- src/rxMisc/docs/rxMisc.midway.js
+++ src/rxMisc/docs/rxMisc.midway.js
@@ -136,5 +136,59 @@ describe('rxMisc', function () {
                 expect(fn('$1.10')).to.equal(110);
             });
         });
+
+        describe('unless', function () {
+            var unless;
+            var elem;
+            var message;
+
+            before(function () {
+                autoSaving.form = {
+                    checkbox: false,
+                    name: '',
+                    description: '',
+                    sensitiveData: ''
+                };
+                message = 'I am possibly not un-authorizing the above: true';
+                unless = encore.rxMisc.unless;
+                elem = element(by.binding('formData.checkbox'));
+                encore.rxMisc.nullValueMatches.push(message);
+            });
+
+            it('should return `null` for non-present elements', function () {
+                expect(unless(elem)).to.eventually.be.null;
+            });
+
+            it('should return a custom fallback value for non-present elements', function () {
+                expect(unless(elem, function () {}, false)).to.eventually.be.false;
+            });
+
+            it('should return `null` for present elements that match null value text', function () {
+                autoSaving.checkbox = true;
+                expect(unless(elem)).to.eventually.be.null;
+            });
+
+            it('should return the text when an element does not match any null value text', function () {
+                encore.rxMisc.nullValueMatches.length = 0;
+                expect(unless(elem)).to.eventually.equal(message);
+            });
+
+            it('should return the result of a custom function', function () {
+                var reverseIt = function (text) {
+                    return text.split('').reverse().join('');
+                };
+                expect(unless(elem, reverseIt)).to.eventually.equal(message.split('').reverse().join(''));
+            });
+
+            it('should return null for elements that are not displayed', function () {
+                element(by.cssContainingText('a', 'Hide this message')).click();
+                expect(unless(elem)).to.eventually.be.null;
+            });
+
+            it('should return a custom fallback value for non-displayed elements', function () {
+                expect(unless(elem, function () {}, false)).to.eventually.be.false;
+            });
+
+        });
     });
 });
