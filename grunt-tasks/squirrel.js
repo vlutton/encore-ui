1e6e6f8685cbf6350cf7cd8c34a1bc24a4f4aec6
Droogans <droogans@gmail.com>
2015-08-12 16:00:23 -0500

    refact(grunt): Responsive shipit squirrel

--- grunt-tasks/squirrel.js
+++ grunt-tasks/squirrel.js
@@ -3,11 +3,17 @@ var _ = require('lodash');
 
 module.exports = function (grunt) {
     grunt.registerTask('squirrel', 'Logs a shipit squirrel', function () {
+        // uniform bubble sizes, regardless of version number
+        var shipIt = grunt.template.process('| Ship it! v<%= pkg.version %>  |');
+        var topBubble = _.repeat('_', shipIt.length - 5);
+        var topAir = ['/', _.repeat(' ', shipIt.length - 3), '\\'].join('');
+        var bottomBubble = [_.repeat('_', shipIt.length - 3), '/'].join('');
+
         var squirrels = [
-            ['         _-------_        _______________',
-             '       .\'  ..::. `\\      /               \\',
-             '      /  .::\' `\'` /     | Ship it! v<%= pkg.version %> |',
-             '     / .::\' .--.=;     / ________________/',
+            ['         _-------_        ' + topBubble,
+             '       .\'  ..::. `\\     ' + topAir,
+             '      /  .::\' `\'` /     ' + shipIt,
+             '     / .::\' .--.=;     / ' + bottomBubble,
              '     | ::\' /  C ..\\   / /',
              '     | :: |   \\  _.) <_/',
              '      \\ \':|   /  \\',
@@ -15,10 +21,10 @@ module.exports = function (grunt) {
              '          `-|   );/',
              '             \'--\'-\'\''],
             ['                     ^',
-             '         _-------_  /T\\      _______________',
-             '       .\'  ..::. `\\ |||     /               \\',
-             '      /  .::\' `\'` / |||    | Ship it! v<%= pkg.version %> |',
-             '     / .::\' .--.=;  |||   / ________________/',
+             '         _-------_  /T\\      ' + topBubble,
+             '       .\'  ..::. `\\ |||    ' + topAir,
+             '      /  .::\' `\'` / |||    ' + shipIt,
+             '     / .::\' .--.=;  |||   / ' + bottomBubble,
              '     | ::\' /  C ..\\ |||  / /',
              '     | :: |   \\  _.)||| <_/',
              '      \\ \':|   /  \\  |||',
