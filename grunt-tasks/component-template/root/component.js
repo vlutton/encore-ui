1403bbe1131ff9479ad47490fd86e355849ab6c5
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-09-09 16:18:20 -0500

    chore(componentTemplate): update for standard component ngdocs

--- grunt-tasks/component-template/root/component.js
+++ grunt-tasks/component-template/root/component.js
@@ -1,1 +1,16 @@
+/**
+ * @ngdoc overview
+ * @name {%= name %}
+ * @description
+ * # {%= name %} Component
+ *
+ * ## Services
+ * * Links to service APIs provided by {%= name %} component.
+ *
+ * ## Directives
+ * * Links to directive APIs provided by {%= name %} component.
+ *
+ * ## Related Directives (if applicable)
+ * * Links to directive APIs provided by other components.
+ */
 angular.module('encore.ui.{%= name %}', []);
