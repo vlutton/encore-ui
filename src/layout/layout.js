c78e098b4cca4e4df08ef9dac026541339f7c966
bry4n <bryann83@gmail.com>
2015-09-16 17:09:51 -0500

    feat(docs): FRMW-153 - Migrate layout API Docs in README to ngdocs

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -4,6 +4,19 @@
  * @description
  * # layout Component
  *
- * [TBD]
+ * Encore UI includes a grid system forked from
+ * [Angular Material's layout module](https://material.angularjs.org/#/layout/container)
+ * with minor usability enhancements to provide an assortment of attribute-based
+ * layout options based on the flexbox layout model. Included are intuitive attribute
+ * based styles that ease the creation of responsive row and/or column based page layouts.
+ *
+ * ## Note About Responsive Features
+ *
+ * Two versions of the Encore UI CSS file are included in this project. One includes
+ * responsive design style attributes (encore-ui-resp-x.x.x.css). The other omits
+ * these attributes to save space if desired (encore-ui-x.x.x.css). Be sure to only
+ * include the appropriate css file for your project. Any attributes which include
+ * the following suffixes require the responsive css file to work:
+ * '-sm', '-gt-sm', '-md', '-gt-md', '-lg', '-gt-lg'.
  */
 angular.module('encore.ui.layout', []);
