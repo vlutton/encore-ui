2212cb8f52e5f0fb63baeb2059a62b48ae41c5ce
Freddy Knuth <freddy.knuth@rackspace.com>
2015-09-22 17:49:25 -0500

    feat(rxFeedback): Adding a controller to the feedback modal

--- src/rxFeedback/rxFeedback.js
+++ src/rxFeedback/rxFeedback.js
@@ -114,6 +114,29 @@ angular.module('encore.ui.rxFeedback', ['ngResource'])
     return container;
 })
 /**
+ * @ngdoc controller
+ * @name rxFeedback.controller:rxFeedbackController
+ * @scope
+ * @description
+ * Allows the customization of the feedback modal via `$scope` and `$modalInstance`.
+ */
+.controller('rxFeedbackController', function ($scope, $modalInstance, $rootScope, $injector) {
+    // define a controller for the modal to use
+    $scope.submit = function () {
+        $modalInstance.close($scope);
+    };
+
+    $scope.cancel = $modalInstance.dismiss;
+
+    // cancel out of the modal if the route is changed
+    $rootScope.$on('$routeChangeSuccess', $modalInstance.dismiss);
+
+    // Allow external overrides of the form
+    if ($injector.has('FeedbackService')) {
+        $injector.get('FeedbackService').initialize($scope, $modalInstance);
+    }
+})
+/**
  * @ngdoc directive
  * @name rxFeedback.directive:rxFeedback
  * @restrict E
