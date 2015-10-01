6d231a59f7be0e221c0b5a5a2ca82463c035df97
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-10-01 15:18:42 -0500

    chore(rxFloatingHeader): add Y tolerance to midways
    * add Y tolerance
    * moved rxFloatingHeader utility functions to rxMisc
    * aliased utility functions to their rxMisc counterparts
      (for backward compatibility)

--- src/rxFloatingHeader/rxFloatingHeader.page.js
+++ src/rxFloatingHeader/rxFloatingHeader.page.js
@@ -1,81 +1,46 @@
 /*jshint node:true*/
-var _ = require('lodash');
 
 /**
-   @exports encore.rxFloatingHeader
+ * @exports encore.rxFloatingHeader
  */
 exports.rxFloatingHeader = {
-
     /**
-       Accepts an ElementArrayFinder, which can have several locations. Should the
-       list of elements be stacked vertically (say, in a list of table rows),
-       the element with the smallest Y coordinate will be scrolled to.
-       @function
-       @param {WebElement} elem - An element, or a list of elements to scroll to
-       @returns {undefined}
-    */
+     * @deprecated Use rxMisc.scrollToElement
+     * @description
+     * **ALIASED**: Plese use {@link rxMisc.scrollToElement} instead.
+     * This function will be removed in a future release of the framework.
+     */
     scrollToElement: function (elem) {
-        return elem.getLocation().then(function (loc) {
-            if (_.isArray(loc)) {
-                loc = _.min(loc, 'y');
-            }
-
-            var command = ['window.scrollTo(0, ', loc.y.toString(), ');'].join('');
-            browser.executeScript(command);
-        });
+        return encore.rxMisc.scrollToElement(elem);
     },
 
     /**
-       @function
-       @param {WebElement} e1 - First element to compare Y locations against.
-       @param {WebElement} e2 - Second element to compare Y locations against.
-       @returns {Boolean} Whether or not `e1` and `e2` have the same Y coordinates.
+     * @deprecated Use rxMisc.compareYLocations
+     * @description
+     * **ALIASED**: Plese use {@link rxMisc.compareYLocations} instead.
+     * This function will be removed in a future release of the framework.
      */
     compareYLocations: function (e1, e2) {
-        return this.compareLocations(e1, e2, 'y');
+        return encore.rxMisc.compareYLocations(e1, e2);
     },
 
     /**
-       @function
-       @param {WebElement} e1 - First element to compare X locations against.
-       @param {WebElement} e2 - Second element to compare X locations against.
-       @returns {Boolean} Whether or not `e1` and `e2` have the same X coordinates.
-    */
-    compareXLocations: function (e1, e2) {
-        return this.compareLocations(e1, e2, 'x');
-    },
-
-    /**
-      Unify input from either a location object or a web element into a promise
-      representing the location attribute (x or y) of either input.
-      Both `transformLocation($('.element'), 'y')` and `transformLocation({x: 20, y: 0}, 'y')`
-      return a promise representing the y value of the resulting (or provided) location object.
-      @private
-    */
-    transformLocation: function (elementOrLocation, attribute) {
-        if (_.isFunction(elementOrLocation.getLocation)) {
-            var elem = elementOrLocation;
-            return elem.getLocation().then(function (loc) {
-                return loc[attribute];
-            });
-        } else {
-            var location = elementOrLocation;
-            if (_.has(location, attribute)) {
-                return protractor.promise.fulfilled(location[attribute]);
-            } else {
-                return protractor.promise.fulfilled(location);
-            }
-        }
-    },
-
-    /**
-       @private
+     * @deprecated Use rxMisc.compareXLocations
+     * @description
+     * **ALIASED**: Plese use {@link rxMisc.compareXLocations} instead.
+     * This function will be removed in a future release of the framework.
      */
-    compareLocations: function (e1, e2, attribute) {
-        var promises = [this.transformLocation(e1, attribute), this.transformLocation(e2, attribute)];
-        return protractor.promise.all(promises).then(function (locations) {
-            return locations[0] === locations[1];
-        });
-    }
+    compareXLocations: function (e1, e2) {
+        return encore.rxMisc.compareXLocations(e1, e2);
+    },
 
+    /**
+     * @deprecated Use rxMisc.transformLocation
+     * @description
+     * **ALIASED**: Plese use {@link rxMisc.transformLocation} instead.
+     * This function will be removed in a future release of the framework.
+     */
+    transformLocation: function (elementOrLocation, attribute) {
+        return encore.rxMisc.transformLocation(elementOrLocation, attribute);
+    }
 };
--- src/rxMisc/rxMisc.page.js
+++ src/rxMisc/rxMisc.page.js
@@ -1,4 +1,5 @@
 /*jshint node:true*/
+var _ = require('lodash');
 
 /**
  * @namespace
@@ -177,6 +178,86 @@ exports.rxMisc = {
                 return times;
             }
         });
-    }
+    },
 
+    /**
+     * Accepts an ElementArrayFinder, which can have several locations. Should the
+     * list of elements be stacked vertically (say, in a list of table rows),
+     * the element with the smallest Y coordinate will be scrolled to.
+     * @function
+     * @param {WebElement} elem An element, or a list of elements to scroll to
+     * @returns {undefined}
+     */
+    scrollToElement: function (elem) {
+        return elem.getLocation().then(function (loc) {
+            if (_.isArray(loc)) {
+                loc = _.min(loc, 'y');
+            }
+
+            var command = ['window.scrollTo(0, ', loc.y.toString(), ');'].join('');
+            browser.executeScript(command);
+        });
+    },
+
+    /**
+     * @function
+     * @param {WebElement} e1 First element to compare Y locations against.
+     * @param {WebElement} e2 Second element to compare Y locations against.
+     * @returns {Boolean} Whether or not `e1` and `e2` have the same Y coordinates.
+     */
+    compareYLocations: function (e1, e2) {
+        return this.compareLocations(e1, e2, 'y');
+    },
+
+    /**
+     * @function
+     * @param {WebElement} e1 First element to compare X locations against.
+     * @param {WebElement} e2 Second element to compare X locations against.
+     * @returns {Boolean} Whether or not `e1` and `e2` have the same X coordinates.
+     */
+    compareXLocations: function (e1, e2) {
+        return this.compareLocations(e1, e2, 'x');
+    },
+
+    /**
+     * @function
+     * @description
+     * Unify input from either a location object or a web element into a promise
+     * representing the location attribute (x or y) of either input.
+     * Both `transformLocation($('.element'), 'y')` and `transformLocation({x: 20, y: 0}, 'y')`
+     * return a promise representing the y value of the resulting (or provided) location object.
+     */
+    transformLocation: function (elementOrLocation, attribute) {
+        if (_.isFunction(elementOrLocation.getLocation)) {
+            var elem = elementOrLocation;
+            return elem.getLocation().then(function (loc) {
+                return loc[attribute];
+            });
+        } else {
+            var location = elementOrLocation;
+            if (_.has(location, attribute)) {
+                return protractor.promise.fulfilled(location[attribute]);
+            } else {
+                return protractor.promise.fulfilled(location);
+            }
+        }
+    },
+
+    /**
+     * @private
+     * @function
+     * @param {WebElement} e1 First element to compare locations against.
+     * @param {WebElement} e2 Second element to compare locations against.
+     * @param {String} attribute attribute to compare ('x' or 'y')
+     */
+    compareLocations: function (e1, e2, attribute) {
+        var promises = [
+            this.transformLocation(e1, attribute),
+            this.transformLocation(e2, attribute)
+        ];
+
+        return protractor.promise.all(promises).then(function (locations) {
+            return locations[0] === locations[1];
+        });
+    }
 };
