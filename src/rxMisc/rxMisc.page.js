352ef11804ebdcd21e7b3530cd5d6254fb75e4d6
Tom Reece <tomreece@gmail.com>
2015-10-06 10:54:51 -0500

    test(rxMisc): Remove a test and update currencyToPennies ngdoc

--- src/rxMisc/rxMisc.page.js
+++ src/rxMisc/rxMisc.page.js
@@ -22,17 +22,14 @@ exports.rxMisc = {
     /**
      * @description
      * Transform `currencyString` (USD) to an integer representing pennies. Built to reverse
-     * Angular's 'currency' filter. If your currency string includes fractions of a penny,
-     * that precision will be lost!
+     * Angular's 'currency' filter. Do not pass in fractions of a penny.
      * @param {string} currencyString - Raw text as output by Angular's `currency` filter.
      *
      * @example
      * ```js
      * encore.rxMisc.currencyToPennies('$0.01')      ==  1
-     * encore.rxMisc.currencyToPennies('$0.019')     ==  1
      * encore.rxMisc.currencyToPennies('$100 CAN')   ==  10000
      * encore.rxMisc.currencyToPennies('($100 AUS)') == -10000
-     * encore.rxMisc.currencyToPennies('($1.011)')   == -101
      * encore.rxMisc.currencyToPennies('$1.10')      ==  110
      * ```
      */
