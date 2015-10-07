2dae031d969477ef801a3cba68236d18f4f41fea
bry4n <bryann83@gmail.com>
2015-10-07 13:08:46 -0500

    feat(docs): Migrate tabs docs

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -4,6 +4,20 @@
  * @description
  * # tabs Component
  *
- * [TBD]
+ * This component provides styles and a demo for the [the Angular-UI Bootstrap
+ * Tabs plugin](https://github.com/angular-ui/bootstrap/tree/master/src/tabs),
+ * which is included as a dependency for EncoreUI.
+ *
+ * ## Usage
+ *
+ * Usage is the exact same as demoed on the Angular-UI Bootstrap site. See
+ * [the Angular-UI Bootstrap Docs](http://angular-ui.github.io/bootstrap/) for
+ * further guidance on usage and configuration of this component.
+ *
+ * ## Disclaimer
+ *
+ * Only the default horizontal tabs are supported by these styles. `Vertical`,
+ * `Pills` and `Justified` tabs are currently unsupported.
+ *
  */
 angular.module('encore.ui.tabs', []);
