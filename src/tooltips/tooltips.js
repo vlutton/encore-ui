24c74facd10e2d59961fa9c3abff9e6c0ffdc3f3
James Thompson <james.thompson@rackspace.com>
2015-10-07 14:37:02 -0500

    docs(tooltips): Migrate tooltips to API Docs

--- src/tooltips/tooltips.js
+++ src/tooltips/tooltips.js
@@ -4,6 +4,28 @@
  * @description
  * # tooltips Component
  *
- * [TBD]
+ * The tooltip component provides styles to raw HTML 
+ * elements and custom directive templates.
+ *
+ * ## Usage
+ * 
+ * Usage is the exact same as shown in the
+ * [Angular-UI Bootstrap Documentation](https://angular-ui.github.io/bootstrap/#/tooltip).
+ * See for further guidance on usage and configuration of this component.
+ * 
+ * **NOTE:**  The `tooltip` directive and its optional attributes can **_only_** be 
+ * applied to raw HTML elements. They can't be applied to directives like this:
+ * <pre>
+ * // DOES NOT WORK 
+ * 
+ * <rx-button tooltip="...">
+ * </pre>
+ * 
+ * If you're creating your own custom directive, it's fine to use the `tooltip` 
+ * directive inside of your directive's template.  See the tooltips component 
+ * {@link /#/components/tooltips demo} for example usage.
+ * 
+ * The [Angular-UI Bootstrap Tooltip](https://github.com/angular-ui/bootstrap/tree/master/src/tooltip) 
+ * plugin is included as a dependency for EncoreUI.
  */
 angular.module('encore.ui.tooltips', []);
