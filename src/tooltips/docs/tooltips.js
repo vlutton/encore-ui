fc91a4d599d5152b66ce5fa162bc124ad1e02479
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-10-16 01:22:36 -0500

    fix(tooltips): styling corrections and refactoring
    * Because ng-bootstrap only supports `top`, `bottom`, `left`, and
      `right` positions, the following positions were removed from styles:
      * `top-left`
      * `top-right`
      * `bottom-left`
      * `bottom-right`
    * Moved tooltips vars to `styles/vars.less`

--- src/tooltips/docs/tooltips.js
+++ src/tooltips/docs/tooltips.js
@@ -2,4 +2,5 @@
 angular.module('demoApp')
 .controller('tooltipsCtrl', function ($scope) {
     $scope.dynamicTooltip = 'I was defined in the controller!';
+    $scope.htmlTooltip = '<span class="tooltip-header">A Tooltip Title</span><p>You can use HTML</p>';
 });
