0fa50245d0d2bef6c25f0f25eebd8e88de3083c0
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-11-02 16:14:29 -0600

    chore(rxBreadcrumbs): decompose monolithic source files

---
+++ src/rxBreadcrumbs/rxBreadcrumbs.module.js
@@ -0,0 +1,17 @@
+/**
+ * @ngdoc overview
+ * @name rxBreadcrumbs
+ * @description
+ * # rxBreadcrumbs Component
+ *
+ * `rxBreadcrumbs` adds or updates page navigation breadcrumbs on a page.
+ *
+ * ## Directives
+ * * {@link rxBreadcrumbs.directive:rxBreadcrumbs rxBreadcrumbs}
+ *
+ * ## Services
+ * * {@link rxBreadcrumbs.service:rxBreadcrumbsSvc rxBreadcrumbsSvc}
+ */
+angular.module('encore.ui.rxBreadcrumbs', [
+    'ngSanitize'
+]);
---
+++ src/rxBreadcrumbs/scripts/rxBreadcrumbs.js
@@ -0,0 +1,45 @@
+angular.module('encore.ui.rxBreadcrumbs')
+/**
+ * @ngdoc directive
+ * @name rxBreadcrumbs.directive:rxBreadcrumbs
+ * @restrict E
+ * @scope
+ * @description
+ * Responsible for drawing the breadcrumbs for a page
+ *
+ *
+ * By default, the first breadcrumb will always have an URL of `'/'` and a name of `'Home'`. This can be changed
+ * with the `rxBreadcrumbsSvc.setHome` method (see {@link rxBreadcrumbs.service:rxBreadcrumbsSvc rxBreadcrumbsSvc}).
+ *
+ * @param {String=} status
+ * The tag to apply to any breadcrumbs with usePageStatusTag:true
+ *
+ * This leverages the tags defined in {@link rxApp} to display status tags directly inside of breadcrumbs.
+ * For a given breadcrumb, `status` will take precedence over `usePageStatusTag`, i.e. it will use a tag defined in
+ * `status` instead of checking for and using a tag for the page.
+ *
+ * @param {Boolean=} [usePageStatusTag=false]
+ * If you set it to `true`,
+ * then the breadcrumb will use whatever status tag was passed to page, i.e.:
+ * <pre>
+ * <rx-page status="alpha">
+ * </pre>
+ * This will cause any breadcrumb marked with `usePageStatusTag` on this page to receive the `"alpha"` status tag.
+ *
+ * @example
+ * <pre>
+ * <rx-app site-title="Custom Title"></rx-app>
+ * </pre>
+ */
+.directive('rxBreadcrumbs', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/rxBreadcrumbs.html',
+        controller: function ($scope, rxBreadcrumbsSvc) {
+            $scope.breadcrumbs = rxBreadcrumbsSvc;
+        },
+        scope: {
+            status: '@'
+        }
+    };
+});
