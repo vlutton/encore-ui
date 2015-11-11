24fb706223f93b709dcf83ba7c44ecd73f0c6787
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-11-11 13:57:21 -0600

    chore(rxCapitalize): decompose monolithic source

---
+++ src/rxCapitalize/rxCapitalize.module.js
@@ -0,0 +1,12 @@
+/**
+ * @ngdoc overview
+ * @name rxCapitalize
+ * @description
+ * # rxCapitalize Component
+ *
+ * This component provides logic to capitalize the first word in a string via an Angular filter.
+ *
+ * ## Filters
+ * * {@link rxCapitalize.filter:rxCapitalize rxCapitalize}
+ */
+angular.module('encore.ui.rxCapitalize', []);
