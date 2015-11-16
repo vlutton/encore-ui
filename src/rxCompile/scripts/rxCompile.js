057ea9d204c3e6976535f1b9d64d4bad2b49c1b7
James Thompson <james.thompson@rackspace.com>
2015-11-16 14:58:23 -0600

    chore(rxCompile): FRMW-487 decompose monolithic source

--- src/rxCompile/scripts/rxCompile.js
+++ src/rxCompile/scripts/rxCompile.js
@@ -1,15 +1,4 @@
-/**
- * @ngdoc overview
- * @name rxCompile
- * @description
- * # rxCompile Component
- *
- * A component that allows compilation of expressions inside of other expressions.
- *
- * ## Directives
- * * {@link rxCompile.directive:rxCompile rxCompile}
- */
-angular.module('encore.ui.rxCompile', [])
+angular.module('encore.ui.rxCompile')
 /**
  * @ngdoc directive
  * @name rxCompile.directive:rxCompile
