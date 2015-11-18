d2a582ee7ede6b7a6d12e2cf1250f41ef30789ac
James Thompson <james.thompson@rackspace.com>
2015-11-18 16:13:28 -0600

    chore(rxCharacterCount): FRMW-484-monolithic decomposition

---
+++ src/rxCharacterCount/rxCharacterCount.module.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc overview
+ * @name rxCharacterCount
+ * @description
+ * # rxCharacterCount Component
+ *
+ * A component designed for `<textarea>` elements that provides character count
+ * functionality.
+ *
+ * ## Directives
+ * * {@link rxCharacterCount.directive:rxCharacterCount rxCharacterCount}
+ */
+angular.module('encore.ui.rxCharacterCount', []);
