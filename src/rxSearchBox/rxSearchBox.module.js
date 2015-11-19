e7ddbf69d70bc67389d30950f037effcf72831ce
James Thompson <james.thompson@rackspace.com>
2015-11-19 14:00:25 -0600

    chore(rxSearchBox): FRMW-496-monolithic decomposition

---
+++ src/rxSearchBox/rxSearchBox.module.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc overview
+ * @name rxSearchBox
+ * @description
+ * # rxSearchBox Component
+ *
+ * The rxSearchBox component provides functionality around creating a search
+ * input box.
+ *
+ * ## Directives
+ * * {@link rxSearchBox.directive:rxSearchBox rxSearchBox}
+ */
+angular.module('encore.ui.rxSearchBox', []);
