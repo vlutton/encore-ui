710b46851397a12a0fccb3aca0c1966bdd7e4c50
James Thompson <james.thompson@rackspace.com>
2015-11-19 11:37:33 -0600

    chore(rxInfoPanel): FRMW-492-monolithic decomposition

---
+++ src/rxInfoPanel/rxInfoPanel.module.js
@@ -0,0 +1,12 @@
+/**
+ * @ngdoc overview
+ * @name rxInfoPanel
+ * @description
+ * # rxInfoPanel Component
+ *
+ * This is a generic info panel intended for use at the top of pages.
+ *
+ * ## Directives
+ * * {@link rxInfoPanel.directive:rxInfoPanel rxInfoPanel}
+ */
+angular.module('encore.ui.rxInfoPanel', []);
