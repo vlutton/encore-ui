2f149bb65da88593f5a593cff52b275f2081088b
Ryan A. Johnson <ryan.johnson@rackspace.com>
2015-11-19 11:15:37 -0600

    chore(rxFavicon): decompose monolithic source

--- src/rxFavicon/scripts/rxFavicon.js
+++ src/rxFavicon/scripts/rxFavicon.js
@@ -1,15 +1,4 @@
-/**
- * @ngdoc overview
- * @name rxFavicon
- * @description
- * # rxFavicon Component
- *
- * This component allows custom favicons between local, staging and production environments.
- *
- * ## Directives
- * * {@link rxFavicon.directive:rxFavicon rxFavicon}
- */
-angular.module('encore.ui.rxFavicon', ['encore.ui.rxEnvironment'])
+angular.module('encore.ui.rxFavicon')
 /**
  * @ngdoc directive
  * @name rxFavicon.directive:rxFavicon
