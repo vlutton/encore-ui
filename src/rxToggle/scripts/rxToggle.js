2ba08c313d7590abe2990435dfbe0be7601e5e55
James Thompson <james.thompson@rackspace.com>
2015-11-19 14:57:22 -0600

    chore(rxToggle): FRMW-500-monolithic decomposition

--- src/rxToggle/scripts/rxToggle.js
+++ src/rxToggle/scripts/rxToggle.js
@@ -1,16 +1,3 @@
-/**
- * @ngdoc overview
- * @name rxToggle
- * @description
- * # rxToggle Component
- *
- * This component provides an attribute directive to handle toggling a boolean
- * scope property for show/hide purposes (normally used in conjunction with
- * ng-show to toggle hidden content).
- *
- * ## Directives
- * * {@link rxToggle.directive:rxToggle rxToggle}
- */
 angular.module('encore.ui.rxToggle', [])
 /**
  * @ngdoc directive
