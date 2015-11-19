9e18f4b346478fa6c4445ab0addbad4b0d1221d0
James Thompson <james.thompson@rackspace.com>
2015-11-19 11:13:56 -0600

    chore(rxFloatingHeader): FRMW-490-monolithic decomposition

---
+++ src/rxFloatingHeader/rxFloatingHeader.module.js
@@ -0,0 +1,17 @@
+/**
+ * @ngdoc overview
+ * @name rxFloatingHeader
+ * @description
+ * # rxFloatingHeader Component
+ *
+ * `rxFloatingHeader` component turns a table header into floating persistent header so that names of columns are still
+ *  visible, even as a user scrolls down the page.
+ *
+ * `rxFloatingHeader` is also fully compatible with {@link rxSortableColumn} and {@link rxPaginate}.
+ * 
+ * ## Directives
+ * * {@link rxFloatingHeader.directive:rxFloatingHeader rxFloatingHeader}
+ */
+angular.module('encore.ui.rxFloatingHeader', [
+    'encore.ui.rxMisc'
+]);
